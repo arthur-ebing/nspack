1d6ca77f380c9c9dd5a71f52263c8deba3397622
James Silberbauer <jamessil@telkomsa.net>
2019-09-13 10:55:36 +0200

    Make fruit size migrations reversible

--- db/migrations/201908261345_fruit_size_references_table_change.rb
+++ db/migrations/201908261345_fruit_size_references_table_change.rb
@@ -12,7 +12,7 @@ Sequel.migration do
 
   down do
     alter_table(:fruit_size_references) do
-      add_foreign_key :fruit_actual_counts_for_pack_id, :fruit_actual_counts_for_packs, null: false, key: [:id]
+      add_foreign_key :fruit_actual_counts_for_pack_id, :fruit_actual_counts_for_packs # , null: false, key: [:id]
       drop_constraint(:fruit_size_references_idx)
       add_unique_constraint [:fruit_actual_counts_for_pack_id, :size_reference], name: :fruit_size_references_fruit_actual_counts_for_pack_id_size__key
     end
