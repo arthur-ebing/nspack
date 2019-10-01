04eb5550f1585cc93ac2c04b70e291cb0e24e4b5
user <lux@nosoft.biz>
2019-10-01 10:19:06 +0200

    Messcada App: rmt_bin_weighing

---
+++ lib/applets/messcada_applet.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+root_dir = File.expand_path('..', __dir__)
+# Dir["#{root_dir}/messcada/entities/*.rb"].each { |f| require f }
+Dir["#{root_dir}/messcada/interactors/*.rb"].each { |f| require f }
+# # Dir["#{root_dir}/messcada/jobs/*.rb"].each { |f| require f }
+# Dir["#{root_dir}/messcada/repositories/*.rb"].each { |f| require f }
+Dir["#{root_dir}/messcada/repositories/*.rb"].each { |f| require f }
+Dir["#{root_dir}/messcada/services/*.rb"].each { |f| require f }
+# # Dir["#{root_dir}/messcada/task_permission_checks/*.rb"].each { |f| require f }
+# Dir["#{root_dir}/messcada/ui_rules/*.rb"].each { |f| require f }
+Dir["#{root_dir}/messcada/validations/*.rb"].each { |f| require f }
+# Dir["#{root_dir}/messcada/views/**/*.rb"].each { |f| require f }
+
+module MesscadaApp
+end
