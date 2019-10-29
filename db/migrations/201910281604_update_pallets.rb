2b1401ced560b76b986ec0e4df2bc0de90feecba
FritzViljoen <fritzviljoen@outlook.com>
2019-10-29 11:11:24 +0200

    migration update add foreign key load_id to table pallets

--- db/migrations/201910281604_update_pallets.rb
+++ db/migrations/201910281604_update_pallets.rb
@@ -1,13 +1,15 @@
 Sequel.migration do
   up do
-    run 'alter table pallets
-      add constraint pallets_load_id_fkey
-      foreign key (load_id) references loads;'
+    extension :pg_triggers
+    alter_table(:pallets) do
+      add_foreign_key [:load_id], :loads, name: :pallets_load_id_fkey
+    end
   end
 
   down do
-    run 'alter table pallets
-      drop constraint pallets_load_id_fkey;'
+    alter_table(:pallets) do
+      drop_foreign_key [:load_id], name: :pallets_load_id_fkey
+    end
   end
 end
 
