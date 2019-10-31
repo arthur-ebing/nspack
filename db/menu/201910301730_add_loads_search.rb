57440cca3c9f5357e9e39edc0648180ab9f9a4d3
FritzViljoen <fritzviljoen@outlook.com>
2019-10-31 09:31:32 +0200

    changed menu migration used change_program_function rather than drop/add

--- db/menu/201910301730_add_loads_search.rb
+++ db/menu/201910301730_add_loads_search.rb
@@ -1,19 +1,16 @@
 Crossbeams::MenuMigrations::Migrator.migration('Nspack') do
   up do
-    add_program_function 'List Voyages', functional_area: 'Finished Goods', program: 'Dispatch', url: '/list/voyages', seq: 1
+    change_program_function 'Voyages', functional_area: 'Finished Goods', program: 'Dispatch', rename: 'List Voyages', seq: 1
     add_program_function 'Search Voyages', functional_area: 'Finished Goods', program: 'Dispatch', url: '/search/voyages', seq: 2
-    add_program_function 'List Loads', functional_area: 'Finished Goods', program: 'Dispatch', url: '/list/loads', seq: 3
+    change_program_function 'Loads', functional_area: 'Finished Goods', program: 'Dispatch', rename: 'List Loads', seq: 3
     add_program_function 'Search Loads', functional_area: 'Finished Goods', program: 'Dispatch', url: '/search/loads', seq: 4
-    drop_program_function 'Loads', functional_area: 'Finished Goods', program: 'Dispatch'
-    drop_program_function 'Voyages', functional_area: 'Finished Goods', program: 'Dispatch'
+
   end
 
   down do
-    add_program_function 'Voyages', functional_area: 'Finished Goods', program: 'Dispatch', url: '/list/voyages', seq: 1
-    add_program_function 'Loads', functional_area: 'Finished Goods', program: 'Dispatch', url: '/list/loads', seq: 2
-    drop_program_function 'List Loads', functional_area: 'Finished Goods', program: 'Dispatch'
-    drop_program_function 'Search Loads', functional_area: 'Finished Goods', program: 'Dispatch'
-    drop_program_function 'List Voyages', functional_area: 'Finished Goods', program: 'Dispatch'
+    change_program_function 'List Voyages', functional_area: 'Finished Goods', program: 'Dispatch', rename: 'Voyages', seq: 1
     drop_program_function 'Search Voyages', functional_area: 'Finished Goods', program: 'Dispatch'
+    change_program_function 'List Loads', functional_area: 'Finished Goods', program: 'Dispatch', rename: 'Loads', seq: 2
+    drop_program_function 'Search Loads', functional_area: 'Finished Goods', program: 'Dispatch'
   end
 end
