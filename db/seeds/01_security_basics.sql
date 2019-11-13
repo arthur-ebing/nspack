0f2e99a51d2cf5c4e4ba95933872b9e355e574b8
James Silberbauer <jamessil@telkomsa.net>
2019-11-13 08:55:47 +0200

    Seed fixes

--- db/seeds/01_security_basics.sql
+++ db/seeds/01_security_basics.sql
@@ -84,22 +84,3 @@ SELECT g.id,
 (SELECT id FROM security_permissions WHERE security_permission = 'user_permissions')
 FROM security_groups g WHERE g.security_group_name = 'user_permissions';
 
-------------------------
-
-INSERT INTO programs (program_name, program_sequence, functional_area_id)
-VALUES ('Rmd', 1, (SELECT id FROM functional_areas
-                                              WHERE functional_area_name = 'Security'));
-
-INSERT INTO programs_webapps(program_id, webapp) VALUES (
-      (SELECT id FROM programs
-       WHERE program_name = 'Rmd'
-         AND functional_area_id = (SELECT id FROM functional_areas
-                                   WHERE functional_area_name = 'Security')),
-       'Nspack');
-
-
-INSERT INTO program_functions (program_id, program_function_name, url, program_function_sequence)
-VALUES ((SELECT id FROM programs WHERE program_name = 'Rmd'
-         AND functional_area_id = (SELECT id FROM functional_areas
-                                   WHERE functional_area_name = 'Security')),
-         'Registered_mobile_devices', '/list/registered_mobile_devices', 2);
--- db/seeds/10_initial_user_security.sql
+++ db/seeds/10_initial_user_security.sql
@@ -1,11 +1,11 @@
 INSERT INTO programs_users (user_id, program_id, security_group_id)
 VALUES ((SELECT id FROM users ORDER BY id LIMIT 1),
-  (SELECT id FROM programs WHERE program_name = 'menu' AND functional_area_id = (SELECT id FROM functional_areas WHERE functional_area_name = 'security')),
+  (SELECT id FROM programs WHERE program_name = 'Menu' AND functional_area_id = (SELECT id FROM functional_areas WHERE functional_area_name = 'Security')),
   (SELECT id FROM security_groups g WHERE g.security_group_name = 'basic'));
 
 INSERT INTO programs_users (user_id, program_id, security_group_id)
 VALUES ((SELECT id FROM users ORDER BY id LIMIT 1),
-  (SELECT id FROM programs WHERE program_name = 'reports' AND functional_area_id = (SELECT id FROM functional_areas WHERE functional_area_name = 'dataminer')),
+  (SELECT id FROM programs WHERE program_name = 'Reports' AND functional_area_id = (SELECT id FROM functional_areas WHERE functional_area_name = 'Dataminer')),
   (SELECT id FROM security_groups g WHERE g.security_group_name = 'basic'));
 
 INSERT INTO programs_users (user_id, program_id, security_group_id)
@@ -17,8 +17,3 @@ INSERT INTO programs_users (user_id, program_id, security_group_id)
 VALUES ((SELECT id FROM users ORDER BY id LIMIT 1),
   (SELECT id FROM programs WHERE program_name = 'Masterfiles' AND functional_area_id = (SELECT id FROM functional_areas WHERE functional_area_name = 'Development')),
   (SELECT id FROM security_groups g WHERE g.security_group_name = 'user_maintainer'));
-
--- INSERT INTO programs_users (user_id, program_id, security_group_id)
--- VALUES ((SELECT id FROM users ORDER BY id LIMIT 1),
---   (SELECT id FROM programs WHERE program_name = 'Fruit' AND functional_area_id = (SELECT id FROM functional_areas WHERE functional_area_name = 'Masterfiles')),
---   (SELECT id FROM security_groups g WHERE g.security_group_name = 'basic'));
