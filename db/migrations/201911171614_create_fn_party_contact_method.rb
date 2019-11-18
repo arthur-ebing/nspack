f6f5bf021fb4b08533c34122d1d7eff05daa704e
James Silberbauer <jamessil@telkomsa.net>
2019-11-18 06:27:35 +0200

    Change fn_party_contact_method to return an aggregation of values

--- db/migrations/201911171614_create_fn_party_contact_method.rb
+++ db/migrations/201911171614_create_fn_party_contact_method.rb
@@ -5,13 +5,12 @@ Sequel.migration do
       CREATE OR REPLACE FUNCTION public.fn_party_contact_method(in_id integer, in_method_type text)
         RETURNS text AS
       $BODY$
-        SELECT contact_methods.contact_method_code
+        SELECT string_agg(contact_methods.contact_method_code, ', ')
         FROM party_contact_methods
         JOIN contact_methods ON contact_methods.id = party_contact_methods.contact_method_id
         JOIN contact_method_types ON contact_method_types.id = contact_methods.contact_method_type_id
         WHERE party_contact_methods.party_id = in_id
-          AND contact_method_types.contact_method_type = in_method_type
-        LIMIT 1
+          AND contact_method_types.contact_method_type = in_method_type;
       $BODY$
         LANGUAGE sql VOLATILE
         COST 100;
