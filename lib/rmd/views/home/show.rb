a41f72f9b2f347e534aae20e23dcace386cd3f17
James Silberbauer <jamessil@telkomsa.net>
2019-12-04 16:08:58 +0200

    Render menu groups in RMD home menu

--- lib/rmd/views/home/show.rb
+++ lib/rmd/views/home/show.rb
@@ -1,7 +1,7 @@
 module Rmd
   module Home
     class Show
-      def self.call(menu_items)
+      def self.call(menu_items) # rubocop:disable Metrics/AbcSize
         rules = {}
 
         layout = Crossbeams::Layout::Page.build(rules) do |page|
@@ -10,8 +10,15 @@ module Rmd
           menu_items[:programs].each do |_, prog|
             prog.each do |prg|
               page.add_text %(<h2 class="ma0">#{prg[:name]}</h2>)
-              menu_items[:program_functions][prg[:id]].each do |pf|
-                page.add_text %(<a href="#{pf[:url]}" class="f6 link dim br2 ph3 pv2 dib white bg-green mt2 w5 mw6">#{pf[:name]}</a>)
+              combin = menu_items[:program_functions][prg[:id]].map { |r| [r[:group_name], r[:url], r[:name]] }
+              grp = nil
+              combin.each do |group, url, name|
+                if group != grp
+                  page.add_text %(<h3 class="gray ml3 mb0 mt1">#{group}</h3>) unless group.nil?
+                  grp = group
+                end
+                group_indent = group.nil? ? '' : ' ml4'
+                page.add_text %(<a href="#{url}" class="f6 link dim br2 ph3 pv2 dib white bg-green mt2 w5 mw6#{group_indent}">#{name}</a>)
               end
             end
           end
