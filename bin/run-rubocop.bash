4707c0d66f4b8b3044c3e808e6ed5100a4ec2113
James Silberbauer <jamessil@telkomsa.net>
2019-12-30 08:36:08 +0200

    Speed up RuboCop checks before git commit

--- bin/run-rubocop.bash
+++ bin/run-rubocop.bash
@@ -5,4 +5,5 @@ set -e
 cd "${0%/*}/.."
 
 echo "Running rubocop"
-bundle exec rubocop
+# bundle exec rubocop
+bundle exec rubocop $(git status -su | awk '{sub(/^(R.*-> )|[ M?]+/,"")};1' | awk '!/^D/')
