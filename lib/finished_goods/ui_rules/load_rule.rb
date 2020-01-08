3a750ffabbb58ce9161dbb1e9fa881f7c71e393b
FritzViljoen <fritzviljoen@outlook.com>
2020-01-08 13:39:32 +0200

    reverted rules for loads

--- lib/finished_goods/ui_rules/load_rule.rb
+++ lib/finished_goods/ui_rules/load_rule.rb
@@ -256,11 +256,13 @@ module UiRules
     end
 
     def add_rules # rubocop:disable Metrics/AbcSize
-      rules[:can_unship] = @form_object.shipped &&
-                           Crossbeams::Config::UserPermissions.can_user?(@options[:user], :load, :can_unship)
-      rules[:can_ship] = !@form_object.shipped &&
-                         Crossbeams::Config::UserPermissions.can_user?(@options[:user], :load, :can_ship) &&
-                         !FinishedGoodsApp::LoadVehicleRepo.new.find_load_vehicle_from(load_id: @form_object.id).nil_or_empty?
+      if %i[edit ship].include? @mode
+        rules[:can_unship] = @form_object.shipped &&
+                             Crossbeams::Config::UserPermissions.can_user?(@options[:user], :load, :can_unship)
+        rules[:can_ship] = !@form_object.shipped &&
+                           Crossbeams::Config::UserPermissions.can_user?(@options[:user], :load, :can_ship) &&
+                           !FinishedGoodsApp::LoadVehicleRepo.new.find_load_vehicle_from(load_id: @form_object.id).nil_or_empty?
+      end
       rules[:allocated] = @form_object.allocated
       rules[:has_container] = !@form_object.container_code.nil_or_empty?
     end
