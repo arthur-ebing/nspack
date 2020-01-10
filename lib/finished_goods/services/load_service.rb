4e6e5b756db70f51e529be52fee63df032aa8021
FritzViljoen <fritzviljoen@outlook.com>
2020-01-10 14:32:50 +0200

    refactored load service

--- lib/finished_goods/services/load_service.rb
+++ lib/finished_goods/services/load_service.rb
@@ -2,103 +2,129 @@
 
 module FinishedGoodsApp
   class LoadService < BaseService
-    attr_reader :user_name, :params, :voyage_id, :load_id
+    attr_reader :params, :load_id, :load_voyage_id
 
-    def initialize(load_id, params, user_name)
+    def initialize(mode:, params:, user:)
+      @mode = mode
       @params = params.to_h
-      @load_id = load_id
-      @user_name = user_name
+      @load_id = @params[:load_id]
+      @user = user
     end
 
     def call
-      find_or_create_voyage
-      create_or_update_load
-      create_or_update_load_voyage
-      update_pallets_shipped_at unless @params[:shipped_at].nil?
+      res = validate_service_params(params)
+      return validation_failed_response(res) unless res.messages.empty?
 
-      success_response('ok', load_id)
+      return create_call if @mode == :create
+
+      update_call if @mode == :update
     end
 
     private
 
-    def find_or_create_voyage
-      voyage_attrs = params.slice(:voyage_type_id, :vessel_id, :voyage_number, :year)
-      args = voyage_attrs
-      args[:active] = true
-      args[:completed] = false
+    def create_call
+      res = find_voyage
+      create_voyage unless res.success
 
-      @voyage_id = repo.get_with_args(:voyages, :id, args)
-      return unless @voyage_id.nil?
+      create_load
+      create_load_voyage
 
-      @voyage_id = repo.create(:voyages, voyage_attrs)
-      repo.log_status(:voyages, voyage_id, 'CREATED', user_name: user_name)
+      instance = load_entity(load_id)
+      success_response("Created load: #{load_id}", instance)
     end
 
-    def find_or_create_voyage_port(args)
-      voyage_port_id = repo.get_with_args(:voyage_ports, :id, args)
-      return voyage_port_id unless voyage_port_id.nil?
+    def update_call
+      res = find_voyage
+      create_voyage unless res.success
 
-      voyage_port_id = repo.create(:voyage_ports, args)
-      repo.log_status(:voyage_ports, voyage_port_id, 'CREATED', user_name: user_name)
+      update_load
+      update_pallets_shipped_at unless @params[:shipped_at].nil?
+      update_load_voyage
+
+      instance = load_entity(load_id)
+      success_response("Updated load: #{load_id}", instance)
+    end
+
+    def find_voyage
+      params[:active] = true
+      params[:completed] = false
+      @params = params.merge(VoyageRepo.new.find_voyage_with_ports(params))
+      return ok_response unless params[:voyage_id].nil?
+
+      failed_response('Voyage not found.')
+    end
+
+    def create_voyage  # rubocop:disable Metrics/AbcSize
+      params[:voyage_id] = repo.create(:voyages, validate_voyage_params(params))
+      repo.log_status(:voyages, params[:voyage_id], 'CREATED', user_name: @user.user_name)
+
+      pol_port_type_id = repo.get_with_args(:port_types, :id, port_type_code: AppConst::PORT_TYPE_POL)
+      attrs = { voyage_id: params[:voyage_id], port_id: params[:pol_port_id], port_type_id: pol_port_type_id }
+      params[:pol_voyage_port_id] = create_voyage_port(attrs)
+
+      pod_port_type_id = repo.get_with_args(:port_types, :id, port_type_code: AppConst::PORT_TYPE_POD)
+      attrs = { voyage_id: params[:voyage_id], port_id: params[:pod_port_id], port_type_id: pod_port_type_id }
+      params[:pod_voyage_port_id] = create_voyage_port(attrs)
+    end
+
+    def create_voyage_port(attrs)
+      voyage_port_id = repo.create(:voyage_ports, attrs)
+      repo.log_status(:voyage_ports, voyage_port_id, 'CREATED', user_name: @user.user_name)
       voyage_port_id
     end
 
-    def create_or_update_load # rubocop:disable Metrics/AbcSize
-      load_attrs = params.slice(:customer_party_role_id,
-                                :exporter_party_role_id,
-                                :billing_client_party_role_id,
-                                :consignee_party_role_id,
-                                :final_receiver_party_role_id,
-                                :order_number,
-                                :customer_order_number,
-                                :customer_reference,
-                                :shipped_at,
-                                :depot_id,
-                                :exporter_certificate_code,
-                                :final_destination_id,
-                                :transfer_load)
-
-      pol_port_type_id = repo.get_with_args(:port_types, :id, port_type_code: AppConst::PORT_TYPE_POL)
-      pod_port_type_id = repo.get_with_args(:port_types, :id, port_type_code: AppConst::PORT_TYPE_POD)
-      load_attrs[:pol_voyage_port_id] = find_or_create_voyage_port(voyage_id: voyage_id,
-                                                                   port_id: params[:pol_port_id],
-                                                                   port_type_id: pol_port_type_id)
-      load_attrs[:pod_voyage_port_id] = find_or_create_voyage_port(voyage_id: voyage_id,
-                                                                   port_id: params[:pod_port_id],
-                                                                   port_type_id: pod_port_type_id)
-      if @load_id.nil?
-        @load_id = repo.create(:loads, load_attrs)
-        repo.log_status(:loads, load_id, 'CREATED', user_name: user_name)
-      else
-        repo.update(:loads, load_id, load_attrs)
-        repo.log_status(:loads, load_id, 'UPDATED', user_name: user_name)
-      end
+    def create_load
+      @load_id = repo.create(:loads, validate_load_params(params))
+      params[:load_id] = load_id
+      repo.log_status(:loads, load_id, 'CREATED', user_name: @user.user_name)
     end
 
-    def create_or_update_load_voyage # rubocop:disable Metrics/AbcSize
-      load_voyage_attrs = params.slice(:shipping_line_party_role_id,
-                                       :shipper_party_role_id,
-                                       :booking_reference,
-                                       :memo_pad)
-      load_voyage_attrs[:load_id] = load_id
-      load_voyage_attrs[:voyage_id] = voyage_id
+    def update_load
+      repo.update(:loads, load_id, validate_load_params(params))
+      repo.log_status(:loads, load_id, 'UPDATED', user_name: @user.user_name)
+    end
 
+    def create_load_voyage
+      load_voyage_id = repo.create(:load_voyages, validate_load_voyage_params(params))
+      repo.log_status(:load_voyages, load_voyage_id, 'CREATED', user_name: @user.user_name)
+    end
+
+    def update_load_voyage
       load_voyage_id = repo.get_with_args(:load_voyages, :id, load_id: load_id)
-      if load_voyage_id.nil?
-        load_voyage_id = repo.create(:load_voyages, load_voyage_attrs)
-        repo.log_status(:load_voyages, load_voyage_id, 'CREATED', user_name: user_name)
-      else
-        repo.update(:load_voyages, load_voyage_id, load_voyage_attrs)
-        repo.log_status(:load_voyages, load_voyage_id, 'UPDATED', user_name: user_name)
-      end
+      repo.update(:load_voyages, load_voyage_id, validate_load_voyage_params(params))
+      repo.log_status(:load_voyages, load_voyage_id, 'UPDATED', user_name: @user.user_name)
     end
 
     def update_pallets_shipped_at
-      repo.update_pallets_shipped_at(load_id: params[:load_id], shipped_at: params[:shipped_at])
+      repo.update_pallets_shipped_at(load_id: load_id, shipped_at: params[:shipped_at])
     end
 
     def repo
       @repo ||= LoadRepo.new
+    end
+
+    def load_entity(id)
+      repo.find_load_flat(id)
+    end
+
+    def validate_service_params(params)
+      LoadServiceSchema.call(params)
+    end
+
+    def validate_load_params(params)
+      LoadSchema.call(params)
+    end
+
+    def validate_load_voyage_params(params)
+      LoadVoyageSchema.call(params)
+    end
+
+    def validate_voyage_params(params)
+      VoyageSchema.call(params)
+    end
+
+    def validate_voyage_port_params(params)
+      VoyagePortSchema.call(params)
     end
   end
 end
