5f8b881c6404fd04ffad6bcd3a3ee1668c7a18b0
James Silberbauer <jamessil@telkomsa.net>
2020-02-13 21:28:19 +0200

    Drop function still using timestamps without timezone

--- scriptfixes/time_mods.sql
+++ scriptfixes/time_mods.sql
@@ -10,6 +10,9 @@ DROP VIEW vw_packout_details;
 DROP TRIGGER pallet_sequences_prod_run_stats_queue ON public.pallet_sequences;
 DROP TRIGGER pallets_prod_run_stats_queue ON public.pallets;
 
+-- Drop function using timestamps
+DROP FUNCTION public.fn_calc_age_days(integer, timestamp without time zone, timestamp without time zone);
+
 -- Alter all date times to include time zone:
 
 ALTER TABLE audit.current_statuses
@@ -566,6 +569,23 @@ ALTER TABLE users
 ALTER COLUMN created_at TYPE timestamp with time zone,
 ALTER COLUMN updated_at TYPE timestamp with time zone;
 
+
+-- Re-add function:
+
+CREATE OR REPLACE FUNCTION public.fn_calc_age_days(
+    in_id integer,
+    date_from timestamp with time zone,
+    date_to timestamp with time zone)
+  RETURNS double precision AS
+$BODY$
+  SELECT ABS(date_part('epoch', date_from::timestamp - COALESCE(date_to::timestamp, current_timestamp)) / 86400)
+  FROM pallets
+  WHERE id = in_id
+$BODY$
+  LANGUAGE sql VOLATILE
+  COST 100;
+ALTER FUNCTION public.fn_calc_age_days(integer, timestamp with time zone, timestamp with time zone)
+  OWNER TO postgres;
 
 -- Re-create dropped views:
 
