bf67cd2e5bc1117b196a3f3723cfee13af6eb733
James Silberbauer <jamessil@telkomsa.net>
2020-02-17 21:54:02 +0200

    Fixes to orchard fix - including only create deliveries for
    bins that differ from delivery where the delivery also has some
    correct bins.

--- scriptfixes/hbb_fix_rmt_bins_orchard_cultivar_out_of_sync_with_deliveries.rb
+++ scriptfixes/hbb_fix_rmt_bins_orchard_cultivar_out_of_sync_with_deliveries.rb
@@ -14,13 +14,16 @@ class HbbFixRmtBinsOrchardCultivarOutOfSyncWithDeliveries < BaseScript
       SELECT d.id as delivery_id, b.id as bin_id, d.farm_id, d.puc_id, d.date_picked, d.date_delivered, d.delivery_tipped, d.tipping_complete_date_time, d.orchard_id as del_orchard_id , b.orchard_id as bin_orchard_id, d.cultivar_id as del_cultivar_id, b.cultivar_id as bin_cultivar_id
       FROM rmt_deliveries d
       JOIN rmt_bins b on b.rmt_delivery_id = d.id
-      WHERE (d.orchard_id <> b.orchard_id) or (d.cultivar_id <> b.cultivar_id)
+      WHERE (d.orchard_id <> b.orchard_id OR d.cultivar_id <> b.cultivar_id)
+      AND EXISTS(SELECT id FROM rmt_bins WHERE rmt_delivery_id = d.id
+          AND (COALESCE(rmt_bins.orchard_id, 0) = COALESCE(d.orchard_id, 0)
+          AND  COALESCE(rmt_bins.cultivar_id, 0) = COALESCE(d.cultivar_id, 0)))
     SQL
     rmt_delivery_bins = DB[query].all
     p "Bin Records affected: #{rmt_delivery_bins.count}"
 
     text_data = []
-    rmt_delivery_bins.group_by { |h| h[:delivery_id] }.each do |_k, v|
+    rmt_delivery_bins.group_by { |h| h[:delivery_id] }.each do |_k, v| # rubocop:disable Metrics/BlockLength
       if debug_mode
         v.group_by { |h| "#{h[:bin_orchard_id]},#{h[:bin_cultivar_id]}" }.each do |_key, val|
           delivery = val.first
@@ -28,13 +31,13 @@ class HbbFixRmtBinsOrchardCultivarOutOfSyncWithDeliveries < BaseScript
           season_id = rmt_delivery_season(delivery[:bin_cultivar_id], delivery[:date_delivered])
           p "Delivery To Be Created: #{{ tipping_complete_date_time: delivery[:tipping_complete_date_time],
                                          delivery_tipped: delivery[:delivery_tipped],
-                                         date_delivered: delivery[:date_delivered],
+                                         date_delivered: delivery[:date_delivered].to_s,
                                          season_id: season_id,
                                          farm_id: delivery[:farm_id],
                                          puc_id: delivery[:puc_id],
                                          orchard_id: delivery[:bin_orchard_id],
                                          cultivar_id: delivery[:bin_cultivar_id],
-                                         date_picked: delivery[:date_picked],
+                                         date_picked: delivery[:date_picked].to_s,
                                          current: false }}"
         end
       else
@@ -50,11 +53,12 @@ class HbbFixRmtBinsOrchardCultivarOutOfSyncWithDeliveries < BaseScript
                                                          puc_id: delivery[:puc_id],
                                                          orchard_id: delivery[:bin_orchard_id],
                                                          cultivar_id: delivery[:bin_cultivar_id],
-                                                         date_picked: delivery[:date_picked],
+                                                         date_picked: delivery[:date_picked].to_s,
                                                          current: false)
             DB[:rmt_bins].where(id: val.map { |b| b[:bin_id] }).update(rmt_delivery_id: rmt_delivery_id)
-            log_status(:rmt_deliveries, rmt_delivery_id, 'DATA FIX: RMT_DELIVERY_ID', comment: "created from #{rmt_delivery_id}", user_name: 'System')
-            log_multiple_statuses(:rmt_bins, val.map { |b| b[:bin_id] }, 'DATA FIX: RMT_DELIVERY_ID', comment: "changed to #{rmt_delivery_id}", user_name: 'System')
+            log_status(:rmt_deliveries, delivery[:delivery_id], 'DATA FIX: SPLIT UP DELIVERY', comment: "became #{rmt_delivery_id}", user_name: 'System')
+            log_status(:rmt_deliveries, rmt_delivery_id, 'CREATE FROM SPLIT DELIVERY', comment: "created from #{delivery[:delivery_id]}", user_name: 'System')
+            log_multiple_statuses(:rmt_bins, val.map { |b| b[:bin_id] }, 'DATA FIX: SPLIT DELIVERY', comment: "changed from #{delivery[:delivery_id]}", user_name: 'System')
             text_data << "Bins(#{val.map { |b| b[:bin_id] }.join(',')}) : delivery_id from #{delivery[:delivery_id]} to #{rmt_delivery_id}"
           end
         end
@@ -78,7 +82,7 @@ class HbbFixRmtBinsOrchardCultivarOutOfSyncWithDeliveries < BaseScript
       data: updated bins(#{rmt_delivery_bins.map { |b| b[:bin_id] }.join(',')})
 
       text data:
-      #{text_data.join("\n")}
+      #{text_data.join("\n\n")}
     STR
 
     unless rmt_delivery_bins.map { |b| b[:bin_id] }.empty?
@@ -98,9 +102,15 @@ class HbbFixRmtBinsOrchardCultivarOutOfSyncWithDeliveries < BaseScript
   private
 
   def rmt_delivery_season(cultivar_id, date_delivered)
-    DB["SELECT s.*
-        FROM seasons s
-        JOIN cultivars c on c.commodity_id=s.commodity_id
-        WHERE c.id = 4 and ('#{date_delivered}' >= start_date and '#{date_delivered}' <= end_date)"].get(:id)
+    # DB["SELECT s.id
+    #     FROM seasons s
+    #     JOIN cultivars c on c.commodity_id=s.commodity_id
+    #     WHERE c.id = #{cultivar_id} and ('#{date_delivered}' >= start_date and '#{date_delivered}' <= end_date)"].get(:id)
+    DB[:seasons]
+      .join(:cultivars, commodity_id: :commodity_id)
+      .where(Sequel[:cultivars][:id] => cultivar_id)
+      .where { start_date <= date_delivered }
+      .where { end_date >= date_delivered }
+      .get(Sequel[:seasons][:id])
   end
 end
