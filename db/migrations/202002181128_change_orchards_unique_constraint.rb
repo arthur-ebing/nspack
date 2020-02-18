0a57e008b6db6d2116ef5ce39c1bb9a7e47f4e19
miramanz <miraclemanzongo@gmail.com>
2020-02-18 12:07:32 +0200

    Orchards - Change uniqueness on orchards table and UI (validation) - orchard_code + farm_id is the corrrect unique constraint

---
+++ db/migrations/202002181128_change_orchards_unique_constraint.rb
@@ -0,0 +1,16 @@
+
+Sequel.migration do
+  up do
+    alter_table(:orchards) do
+      drop_index :orchard_code, name: :orchards_unique_code
+      add_unique_constraint [:farm_id, :orchard_code], name: :farm_orchard_unique_code
+    end
+  end
+
+  down do
+    alter_table(:orchards) do
+      drop_index [:farm_id, :orchard_code], name: :farm_orchard_unique_code
+      add_unique_constraint [:orchard_code], name: :orchards_unique_code
+    end
+  end
+end
