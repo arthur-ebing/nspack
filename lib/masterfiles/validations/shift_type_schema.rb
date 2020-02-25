4a091af45e6546c3aca5825f5f1c9df49a2801b1
Heila767 <heilahendriks1@gmail.com>
2020-02-25 16:09:32 +0200

    Fix Time slot validations

--- lib/masterfiles/validations/shift_type_schema.rb
+++ lib/masterfiles/validations/shift_type_schema.rb
@@ -2,13 +2,7 @@
 
 module MasterfilesApp
   ShiftTypeSchema = Dry::Validation.Params do
-    configure do
-      config.type_specs = true
-
-      def self.messages
-        super.merge(en: { errors: { base: 'Start hour must be before End hour.' } })
-      end
-    end
+    configure { config.type_specs = true }
 
     optional(:id, :integer).filled(:int?)
     required(:ph_plant_resource_id, :integer).filled(:int?)
@@ -17,9 +11,5 @@ module MasterfilesApp
     required(:start_hour, :integer).filled(:int?, gt?: 0)
     required(:end_hour, :integer).filled(:int?, gt?: 0)
     required(:day_night_or_custom, :string).filled(:str?)
-
-    validate(base: %i[start_hour end_hour]) do |starthr, endhr|
-      starthr < endhr
-    end
   end
 end
