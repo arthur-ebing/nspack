05c69ac5fa51c027a15716aa02bb000b49a93885
FritzViljoen <fritzviljoen@outlook.com>
2020-03-03 16:41:05 +0200

    changes for cultivars registered_code => cultivar_code

--- db/migrations/202002181150_add_otmc_columns.rb
+++ db/migrations/202002181150_add_otmc_columns.rb
@@ -1,15 +1,17 @@
 Sequel.migration do
   up do
     add_column :orchards, :otmc_results, 'hstore'
-    add_column :cultivars, :registered_code, String
+    add_column :cultivars, :cultivar_code, String
     add_column :pallet_sequences, :failed_otmc_results, String
     add_column :pallet_sequences, :pallet_failed_otmc_results, String
+    add_column :pallet_sequences, :phyto_data, String
   end
 
   down do
+    drop_column :pallet_sequences, :phyto_data
     drop_column :pallet_sequences, :failed_otmc_results
     drop_column :pallet_sequences, :pallet_failed_otmc_results
-    drop_column :cultivars, :registered_code
+    drop_column :cultivars, :cultivar_code
     drop_column :orchards, :otmc_results
   end
 end
