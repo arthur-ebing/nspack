512676a1de6d766ed67fef50206309844a0b5e05
FritzViljoen <fritzviljoen@outlook.com>
2020-03-11 15:49:07 +0200

    fix update_pallet_sequences_failed_otmc_results not updating when test changes to passed

---
+++ db/migrations/202003111536_update_function_update_pallet_sequences_failed_otmc_results.rb
@@ -0,0 +1,77 @@
+Sequel.migration do
+  up do
+    run <<~SQL
+      CREATE OR REPLACE FUNCTION public.update_pallet_sequences_failed_otmc_results()
+      RETURNS trigger AS
+      $BODY$
+      BEGIN
+        UPDATE pallet_sequences ps
+        SET failed_otmc_results = sq.failed_otmc_results
+        FROM (
+          SELECT
+              ps.id,
+              CASE
+                  WHEN array_agg(DISTINCT v.test_type_id) = '{null}'
+                  THEN NULL
+                  ELSE array_agg(DISTINCT v.test_type_id)
+              END AS failed_otmc_results
+          FROM pallet_sequences ps
+          LEFT JOIN vw_failed_otmc_results v ON ps.puc_id = v.puc_id
+          AND ps.orchard_id = v.orchard_id
+          AND ps.cultivar_id = v.cultivar_id
+          AND ps.packed_tm_group_id = v.target_market_group_id
+          WHERE ps.failed_otmc_results IS NOT NULL
+          GROUP BY ps.id
+          UNION
+          SELECT
+              ps.id,
+              CASE
+                  WHEN array_agg(DISTINCT v.test_type_id) = '{null}'
+                  THEN NULL
+                  ELSE array_agg(DISTINCT v.test_type_id)
+              END AS failed_otmc_results
+          FROM pallet_sequences ps
+              JOIN vw_failed_otmc_results v ON ps.puc_id = v.puc_id
+              AND ps.orchard_id = v.orchard_id
+              AND ps.cultivar_id = v.cultivar_id
+              AND ps.packed_tm_group_id = v.target_market_group_id
+          GROUP BY ps.id
+        ) AS sq
+        WHERE ps.id = sq.id;
+        RETURN new; 
+      END;
+      $BODY$
+      LANGUAGE plpgsql VOLATILE
+      COST 100;
+    SQL
+
+  end
+
+  down do
+    run <<~SQL
+      CREATE OR REPLACE FUNCTION public.update_pallet_sequences_failed_otmc_results()
+      RETURNS trigger AS
+      $BODY$
+      BEGIN
+        UPDATE pallet_sequences ps
+        SET failed_otmc_results = sq.failed_otmc_results
+        FROM (SELECT 
+                ps.id,
+                array_agg(DISTINCT v.test_type_id) AS failed_otmc_results
+              FROM vw_failed_otmc_results v
+              JOIN pallet_sequences ps 
+                ON ps.puc_id = v.puc_id
+             WHERE ps.orchard_id = v.orchard_id
+               AND ps.cultivar_id = v.cultivar_id
+               AND ps.packed_tm_group_id = v.target_market_group_id
+             GROUP BY ps.id) AS sq
+        WHERE ps.id = sq.id;
+        RETURN new; 
+      END;
+      $BODY$
+      LANGUAGE plpgsql VOLATILE
+      COST 100;
+    SQL
+  end
+end
+
