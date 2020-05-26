648f8a15f8102ea74c8a3ec995fac2bb15d6094c
FritzViljoen <fritzviljoen@outlook.com>
2020-05-26 09:15:35 +0200

    Add status to script

--- scripts/ud_move_pallets_after_unship.rb
+++ scripts/ud_move_pallets_after_unship.rb
@@ -4,19 +4,18 @@
 class LoadUnshipLocationRevert < BaseScript
   def run # rubocop:disable Metrics/AbcSize
     query = <<~SQL
-         SELECT logged_actions.row_data_id AS pallet_id,
-                row_data -> 'pallet_number' AS pallet_number,
-                row_data -> 'location_id'   AS location_id
-
-         FROM audit.logged_actions
-         LEFT JOIN audit.status_logs ON status_logs.transaction_id = logged_actions.transaction_id
-         AND logged_actions.row_data_id = status_logs.row_data_id
-         AND status_logs.table_name = 'pallets'
-         JOIN pallets ON pallets.id = logged_actions.row_data_id
-
-         WHERE logged_actions.table_name = 'pallets'
-         AND logged_actions.transaction_id = 5032061
-         AND pallets.location_id = 5
+      SELECT
+        logged_actions.row_data_id AS pallet_id,
+        row_data -> 'pallet_number' AS pallet_number,
+        row_data -> 'location_id'   AS location_id
+      FROM audit.logged_actions
+      LEFT JOIN audit.status_logs ON status_logs.transaction_id = logged_actions.transaction_id
+        AND logged_actions.row_data_id = status_logs.row_data_id
+        AND status_logs.table_name = 'pallets'
+      JOIN pallets ON pallets.id = logged_actions.row_data_id
+      WHERE logged_actions.table_name = 'pallets'
+        AND logged_actions.transaction_id = 5032061
+        AND pallets.location_id = 5
     SQL
     pallets = DB[query].select_map(%i[pallet_id pallet_number location_id])
 
@@ -29,6 +28,7 @@ class LoadUnshipLocationRevert < BaseScript
         DB.transaction do
           p "Updated row #{pallet_id} #{pallet_number}: #{attrs}"
           DB[:pallets].where(id: pallet_id).update(attrs)
+          log_status(:pallets, pallet_id, 'UNSHIP LOCATION REVERT', comment: "reverted_transaction_id_5032061")
         end
       end
     end
