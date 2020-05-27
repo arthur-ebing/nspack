63dd3edccecd02def2043ec68971af662cd42ae0
James Silberbauer <jamessil@telkomsa.net>
2020-05-27 13:49:29 +0200

    Add PH num and inspection point to UISTK EDI out

--- lib/edi/repositories/uistk_out_repo.rb
+++ lib/edi/repositories/uistk_out_repo.rb
@@ -8,6 +8,8 @@ module EdiApp
           pallet_sequences.pallet_number,
           pallet_sequences.pallet_sequence_number,
           pallet_sequences.production_run_id,
+          plant_resources.plant_resource_code AS packhouse_num,
+          govt_inspection_sheets.inspection_point,
           (SELECT t.treatment_code
             FROM treatments t
             JOIN treatment_types y ON y.id = t.treatment_type_id
@@ -15,6 +17,9 @@ module EdiApp
             AND y.treatment_type_code = 'CHEMICAL_CONTENT' LIMIT 1) AS chemical_content
         FROM pallet_sequences
         JOIN pallets ON pallets.id = pallet_sequences.pallet_id
+        LEFT OUTER JOIN plant_resources ON plant_resources.id = pallet_sequences.packhouse_resource_id
+        LEFT OUTER JOIN govt_inspection_pallets ON govt_inspection_pallets.id = pallets.last_govt_inspection_pallet_id
+        LEFT OUTER JOIN govt_inspection_sheets ON govt_inspection_sheets.id = govt_inspection_pallets.govt_inspection_sheet_id
         WHERE pallets.in_stock
           AND pallet_sequences.marketing_org_party_role_id = ?
       SQL
