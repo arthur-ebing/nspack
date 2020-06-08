243b6067e9a8900b634ebbe6604f7dfd355deae2
miramanz <miraclemanzongo@gmail.com>
2020-06-08 12:04:25 +0200

    Cascading orchard change - Job progress display

--- lib/production/jobs/apply_deliveries_orchard_changes.rb
+++ lib/production/jobs/apply_deliveries_orchard_changes.rb
@@ -3,7 +3,7 @@
 module ProductionApp
   module Job
     class ApplyDeliveriesOrchardChanges < BaseQueJob
-      attr_reader :repo, :user_name
+      attr_reader :repo
 
       self.maximum_retry_count = 0
 
@@ -11,26 +11,21 @@ module ProductionApp
         @repo = ProductionApp::ReworksRepo.new
         change_attrs = params[0][:change_attrs]
         reworks_run_attrs = params[0][:reworks_run_attrs]
-        @user_name = reworks_run_attrs[:user_name]
 
         begin
           repo.transaction do
             res = ProductionApp::ChangeDeliveriesOrchards.call(change_attrs, reworks_run_attrs)
 
-            if res.success
-              send_bus_message('Reworks Cascading orchard change - Applying deliveries orchard changes was successful', message_type: :success, target_user: user_name)
-            else
+            unless res.success
               msg = res.instance.empty? ? res.message : "\n#{res.message}\n#{res.instance}"
               ErrorMailer.send_error_email(subject: 'Apply Deliveries Orchard Changes failed',
                                            message: msg)
-              send_bus_message('Reworks Cascading orchard change - Applying deliveries orchard changes failed', message_type: :error, target_user: user_name)
             end
             finish
           end
         rescue StandardError => e
           log_err(e.message)
           ErrorMailer.send_exception_email(e, subject: 'Apply Deliveries Orchard Changes')
-          send_bus_message("Failed to Apply Deliveries Orchard Changes - #{e.message}", message_type: :error, target_user: user_name)
           expire
         end
       end
