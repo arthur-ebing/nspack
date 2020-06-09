6821c7366375e3475560a24ce878b357378d9059
miramanz <miraclemanzongo@gmail.com>
2020-06-09 09:45:43 +0200

    Reworks Cascading orchard change - add a message on failure and another on success

--- lib/production/jobs/apply_deliveries_orchard_changes.rb
+++ lib/production/jobs/apply_deliveries_orchard_changes.rb
@@ -3,7 +3,7 @@
 module ProductionApp
   module Job
     class ApplyDeliveriesOrchardChanges < BaseQueJob
-      attr_reader :repo
+      attr_reader :repo, :user_name
 
       self.maximum_retry_count = 0
 
@@ -11,12 +11,17 @@ module ProductionApp
         @repo = ProductionApp::ReworksRepo.new
         change_attrs = params[0][:change_attrs]
         reworks_run_attrs = params[0][:reworks_run_attrs]
+        @user_name = reworks_run_attrs[:user_name]
 
         begin
           repo.transaction do
             res = ProductionApp::ChangeDeliveriesOrchards.call(change_attrs, reworks_run_attrs)
 
-            unless res.success
+            if res.success
+              send_bus_message('Reworks Cascading orchard change - Applying deliveries orchard changes was successful', message_type: :information, target_user: user_name)
+
+            else
+              send_bus_message('Reworks Cascading orchard change - Applying deliveries orchard changes failed', message_type: :error, target_user: user_name)
               msg = res.instance.empty? ? res.message : "\n#{res.message}\n#{res.instance}"
               ErrorMailer.send_error_email(subject: 'Apply Deliveries Orchard Changes failed',
                                            message: msg)
