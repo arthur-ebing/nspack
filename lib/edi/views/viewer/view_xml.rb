a3125a06b85e8979e7dc46ed492e1051f0839bb3
FritzViljoen <fritzviljoen@outlook.com>
2020-06-17 12:57:45 +0200

    EDI xml viewer Added back button

--- lib/edi/views/viewer/view_xml.rb
+++ lib/edi/views/viewer/view_xml.rb
@@ -4,10 +4,16 @@ module Edi
   module Viewer
     module File
       class XML
-        def self.call(flow_type, file)
+        def self.call(flow_type, file, back_url:)
           xml = ::File.read(file)
 
           layout = Crossbeams::Layout::Page.build({}) do |page|
+            page.section do |section|
+              section.add_control(control_type: :link,
+                                  text: 'Back',
+                                  url: back_url,
+                                  style: :back_button)
+            end
             page.form_object OpenStruct.new(search: nil)
             page.add_text flow_type.upcase, wrapper: :h3
             page.add_text ::File.basename(file)
