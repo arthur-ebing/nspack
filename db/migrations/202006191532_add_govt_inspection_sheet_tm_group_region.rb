266039ece8af7ca27464c4d333ce9125d36c4f55
FritzViljoen <fritzviljoen@outlook.com>
2020-06-25 14:16:35 +0200

    Updated migrations

--- db/migrations/202006191532_add_govt_inspection_sheet_tm_group_region.rb
+++ db/migrations/202006191532_add_govt_inspection_sheet_tm_group_region.rb
@@ -6,6 +6,18 @@ Sequel.migration do
       add_foreign_key :destination_region_id, :destination_regions
       set_column_allow_null :destination_country_id
     end
+
+    run <<~SQL
+      UPDATE govt_inspection_sheets
+      SET destination_region_id = subquery.destination_region_id
+      FROM (SELECT id, destination_region_id
+            FROM destination_countries) AS subquery
+      WHERE govt_inspection_sheets.destination_country_id = subquery.id;
+    SQL
+
+    alter_table(:govt_inspection_sheets) do
+      set_column_not_null :destination_region_id
+    end
   end
 
   down do
