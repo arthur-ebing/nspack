94ba58f8120b65b6c96ba91d3748eb7add89a1bf
user <lux@nosoft.biz>
2020-07-16 12:11:37 +0200

    Hi Lux,
    Please add Intake date  and  reference number to rmt_deliveries. These fields are not required.
    
    Thank you. Regards

--- lib/raw_materials/test/factories/rmt_delivery_factory.rb
+++ lib/raw_materials/test/factories/rmt_delivery_factory.rb
@@ -9,7 +9,7 @@
 
 module RawMaterialsApp
   module RmtDeliveryFactory
-    def create_rmt_delivery(opts = {})
+    def create_rmt_delivery(opts = {}) # rubocop:disable Metrics/AbcSize
       orchard_id = create_orchard
       cultivar_id = create_cultivar
       rmt_delivery_destination_id = create_rmt_delivery_destination
@@ -25,11 +25,13 @@ module RawMaterialsApp
         farm_id: farm_id,
         puc_id: puc_id,
         truck_registration_number: Faker::Lorem.unique.word,
+        reference_number: Faker::Lorem.unique.word,
         qty_damaged_bins: Faker::Number.number(4),
         qty_empty_bins: Faker::Number.number(4),
         active: true,
         delivery_tipped: false,
         date_picked: '2010-01-01',
+        intake_date: '2010-01-01',
         date_delivered: '2010-01-01 12:00',
         tipping_complete_date_time: '2010-01-01 12:00',
         created_at: '2010-01-01 12:00',
--- lib/raw_materials/validations/rmt_delivery_schema.rb
+++ lib/raw_materials/validations/rmt_delivery_schema.rb
@@ -21,6 +21,6 @@ module RawMaterialsApp
     optional(:auto_allocate_asset_number, :bool).filled(:bool?)
     optional(:quantity_bins_with_fruit, :integer)
     optional(:reference_number, Types::StrippedString).maybe(:str?)
-    required(:intake_date, :date).maybe(:date?)
+    required(:intake_date, :time).maybe(:time?)
   end
 end
