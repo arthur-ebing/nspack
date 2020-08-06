c482de3576c70e283102c300ff279f6325c2054b
James Silberbauer <jamessil@telkomsa.net>
2020-08-06 10:23:08 +0200

    Hold back migration

--- db/migrations/202008051511_remove_pallet_sequences_scrap_trigger.rb
+++ db/migrations/202008051511_remove_pallet_sequences_scrap_trigger.rb
@@ -1,90 +1,92 @@
 Sequel.migration do
   up do
-    run <<~SQL
-      DROP TRIGGER scrap_pallet_sequences ON public.pallet_sequences;
-      DROP FUNCTION public.fn_scrap_pallet_sequences();
-
-      DROP TRIGGER cartons_update_pallet_sequence_id ON public.cartons;
-      DROP FUNCTION public.fn_cartons_carton_quantity_calc();
-    SQL
+    p 'TEMP: Hold this change back'
+    # run <<~SQL
+    #   DROP TRIGGER scrap_pallet_sequences ON public.pallet_sequences;
+    #   DROP FUNCTION public.fn_scrap_pallet_sequences();
+    #
+    #   DROP TRIGGER cartons_update_pallet_sequence_id ON public.cartons;
+    #   DROP FUNCTION public.fn_cartons_carton_quantity_calc();
+    # SQL
   end
 
   down do
-    run <<~SQL
-      CREATE OR REPLACE FUNCTION public.fn_scrap_pallet_sequences()
-      RETURNS trigger AS $BODY$
-    
-      DECLARE
-        seq_carton_quantity INTEGER;
-        plt_sequences_count INTEGER;
-      BEGIN
-        EXECUTE 'SELECT carton_quantity FROM pallet_sequences WHERE id = $1'
-        INTO seq_carton_quantity
-        USING NEW.id;
-
-        EXECUTE 'SELECT count(id) FROM pallet_sequences WHERE pallet_id = $1'
-        INTO plt_sequences_count
-        USING NEW.pallet_id;
-
-        IF (TG_OP = 'UPDATE') THEN 
-           IF (seq_carton_quantity::integer < 1) THEN
-               EXECUTE 'UPDATE pallet_sequences SET removed_from_pallet = true, removed_from_pallet_at = $2, pallet_id = null, 
-                        removed_from_pallet_id = pallet_id, exit_ref = $3
-                        WHERE id = $1'
-               USING NEW.id, current_timestamp, 'SEQUENCE REMOVED BY CARTON TRANSFER';
-           END IF;
-    
-           IF (plt_sequences_count::integer <= 1 AND seq_carton_quantity::integer < 1) THEN
-               EXECUTE 'UPDATE pallets SET scrapped = true, scrapped_at = $2, exit_ref = $3
-                        WHERE id = $1'
-               USING NEW.pallet_id, current_timestamp, 'SCRAPPED';
-           END IF;
-        END IF;
-
-        RETURN NEW;
-      END
-    
-      $BODY$
-        LANGUAGE plpgsql VOLATILE
-        COST 100;
-      ALTER FUNCTION public.fn_scrap_pallet_sequences()
-        OWNER TO postgres;
-  
-      CREATE TRIGGER scrap_pallet_sequences
-        AFTER UPDATE OF carton_quantity
-        ON public.pallet_sequences
-        FOR EACH ROW
-        EXECUTE PROCEDURE fn_scrap_pallet_sequences();
-
-       CREATE OR REPLACE FUNCTION public.fn_cartons_carton_quantity_calc()
-        RETURNS trigger AS $BODY$
-      
-        DECLARE
-         
-        BEGIN
-          IF (TG_OP = 'UPDATE') THEN
-            IF (OLD.pallet_sequence_id IS NOT NULL) THEN
-              EXECUTE 'UPDATE pallet_sequences SET carton_quantity = (carton_quantity - 1)
-                       WHERE id = $1'
-              USING OLD.pallet_sequence_id;
-  
-            END IF;
-          END IF;
-          RETURN NEW;
-        END
-      
-        $BODY$
-          LANGUAGE plpgsql VOLATILE
-          COST 100;
-        ALTER FUNCTION public.fn_cartons_carton_quantity_calc()
-          OWNER TO postgres;
-  
-        CREATE TRIGGER cartons_update_pallet_sequence_id
-          BEFORE UPDATE OF pallet_sequence_id
-          ON public.cartons
-          FOR EACH ROW
-          EXECUTE PROCEDURE fn_cartons_carton_quantity_calc();
-
-    SQL
+    p 'TEMP: Hold this change back'
+    # run <<~SQL
+    #   CREATE OR REPLACE FUNCTION public.fn_scrap_pallet_sequences()
+    #   RETURNS trigger AS $BODY$
+    #
+    #   DECLARE
+    #     seq_carton_quantity INTEGER;
+    #     plt_sequences_count INTEGER;
+    #   BEGIN
+    #     EXECUTE 'SELECT carton_quantity FROM pallet_sequences WHERE id = $1'
+    #     INTO seq_carton_quantity
+    #     USING NEW.id;
+    #
+    #     EXECUTE 'SELECT count(id) FROM pallet_sequences WHERE pallet_id = $1'
+    #     INTO plt_sequences_count
+    #     USING NEW.pallet_id;
+    #
+    #     IF (TG_OP = 'UPDATE') THEN 
+    #        IF (seq_carton_quantity::integer < 1) THEN
+    #            EXECUTE 'UPDATE pallet_sequences SET removed_from_pallet = true, removed_from_pallet_at = $2, pallet_id = null, 
+    #                     removed_from_pallet_id = pallet_id, exit_ref = $3
+    #                     WHERE id = $1'
+    #            USING NEW.id, current_timestamp, 'SEQUENCE REMOVED BY CARTON TRANSFER';
+    #        END IF;
+    #
+    #        IF (plt_sequences_count::integer <= 1 AND seq_carton_quantity::integer < 1) THEN
+    #            EXECUTE 'UPDATE pallets SET scrapped = true, scrapped_at = $2, exit_ref = $3
+    #                     WHERE id = $1'
+    #            USING NEW.pallet_id, current_timestamp, 'SCRAPPED';
+    #        END IF;
+    #     END IF;
+    #
+    #     RETURN NEW;
+    #   END
+    #
+    #   $BODY$
+    #     LANGUAGE plpgsql VOLATILE
+    #     COST 100;
+    #   ALTER FUNCTION public.fn_scrap_pallet_sequences()
+    #     OWNER TO postgres;
+    #
+    #   CREATE TRIGGER scrap_pallet_sequences
+    #     AFTER UPDATE OF carton_quantity
+    #     ON public.pallet_sequences
+    #     FOR EACH ROW
+    #     EXECUTE PROCEDURE fn_scrap_pallet_sequences();
+    #
+    #    CREATE OR REPLACE FUNCTION public.fn_cartons_carton_quantity_calc()
+    #     RETURNS trigger AS $BODY$
+    #   
+    #     DECLARE
+    #      
+    #     BEGIN
+    #       IF (TG_OP = 'UPDATE') THEN
+    #         IF (OLD.pallet_sequence_id IS NOT NULL) THEN
+    #           EXECUTE 'UPDATE pallet_sequences SET carton_quantity = (carton_quantity - 1)
+    #                    WHERE id = $1'
+    #           USING OLD.pallet_sequence_id;
+    #
+    #         END IF;
+    #       END IF;
+    #       RETURN NEW;
+    #     END
+    #   
+    #     $BODY$
+    #       LANGUAGE plpgsql VOLATILE
+    #       COST 100;
+    #     ALTER FUNCTION public.fn_cartons_carton_quantity_calc()
+    #       OWNER TO postgres;
+    #
+    #     CREATE TRIGGER cartons_update_pallet_sequence_id
+    #       BEFORE UPDATE OF pallet_sequence_id
+    #       ON public.cartons
+    #       FOR EACH ROW
+    #       EXECUTE PROCEDURE fn_cartons_carton_quantity_calc();
+    #
+    # SQL
   end
 end
