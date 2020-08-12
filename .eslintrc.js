c408f2a574817b6873e6bc9bc3628e1a74cf6d47
James Silberbauer <jamessil@telkomsa.net>
2020-08-12 17:14:24 +0200

    Diff control - can unnest hashes and have horizontal scrolling

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,41 @@
 module.exports = {
-  "extends": "airbnb-base",
-  "plugins": [
-    "import"
+  extends: 'airbnb-base',
+  plugins: [
+    'import',
   ],
-  "parserOptions": {
-    "sourceType": 'script',
-    "ecmaFeatures": {
-      "impliedStrict": true
-    }
+  parserOptions: {
+    sourceType: 'script',
+    ecmaFeatures: {
+      impliedStrict: true,
+    },
   },
-  "rules": {
-    "no-param-reassign": [ "error", { "props": false } ]
+  rules: {
+    'no-param-reassign': ['error', { props: false }],
+    'arrow-parens': [2, 'as-needed', { requireForBlockBody: true }],
+    'prefer-destructuring': 0,
   },
-  "env": {
-    "browser": true,
-    "jquery": true,
+  env: {
+    browser: true,
+    jquery: true,
   },
-  "globals": {
-    "_": false,
-    "swal": false,
-    "agGrid": false,
-    "Jackbox": false,
-    "Konva": false,
-    "Choices": false,
-    "Selectr": false,
-    "Sortable": false,
-    "multi": false,
-    "HttpError": false,
-    "crossbeamsRmdScan": false,
-    "crossbeamsDialogLevel1": false,
-    "crossbeamsDialogLevel2": false,
-    "crossbeamsGridStore": false,
-    "crossbeamsUtils": false,
-    "crossbeamsDataMinerParams": false,
-    "crossbeamsLocalStorage": false,
-    "crossbeamsGridEvents": false
-  }
+  globals: {
+    _: false,
+    swal: false,
+    agGrid: false,
+    Jackbox: false,
+    Konva: false,
+    Choices: false,
+    Selectr: false,
+    Sortable: false,
+    multi: false,
+    HttpError: false,
+    crossbeamsRmdScan: false,
+    crossbeamsDialogLevel1: false,
+    crossbeamsDialogLevel2: false,
+    crossbeamsGridStore: false,
+    crossbeamsUtils: false,
+    crossbeamsDataMinerParams: false,
+    crossbeamsLocalStorage: false,
+    crossbeamsGridEvents: false,
+  },
 };
