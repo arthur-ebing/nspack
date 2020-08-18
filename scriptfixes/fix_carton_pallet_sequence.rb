37d769eaee538c948f6f24d0ca444a171ae43bf1
miramanz <miraclemanzongo@gmail.com>
2020-08-18 15:21:18 +0200

    Repack pallets - update cartons.pallet_sequence_id for repacked pallets where has_individual_cartons = true

---
+++ scriptfixes/fix_carton_pallet_sequence.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+
+# What this script does:
+# ----------------------
+# Loop through repacked pallets that has_individual_cartons true and update its cartons.pallet_sequence_id
+#
+# Reason for this script:
+# -----------------------
+# update cartons.pallet_sequence_id to repacked pallet_sequence_id if pallet.has_individual_cartons
+#
+class FixCartonPalletSequence < BaseScript
+  def run  # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+    query = <<~SQL
+      SELECT pallet_sequences.id, pallet_sequences.repacked_from_pallet_id
+      FROM pallets
+      JOIN pallet_sequences ON pallets.id = pallet_sequences.pallet_id
+      WHERE pallets.repacked
+      AND pallets.has_individual_cartons;
+    SQL
+    sequences = DB[query].all
+    return failed_response('There are no repacked pallets to fix') if sequences.empty?
+
+    sequence_ids = sequences.map { |r| r[:id] }
+    p "Records affected: #{sequence_ids.count}"
+
+    @carton_ids = []
+
+    sequences.each do |sequence|
+      cartons = DB[:cartons].where(pallet_sequence_id: sequence[:id]).map(:id)
+      next if cartons.count.positive?
+
+      attrs = { pallet_sequence_id: sequence[:id] }
+      repacked_from_pallet_cartons(sequence[:repacked_from_pallet_id], sequence[:pallet_sequence_number])
+
+      next if @carton_ids.count.zero?
+
+      if debug_mode
+        p "Updated cartons #{@carton_ids.join(',')}"
+      else
+        DB.transaction do
+          p "Updated cartons #{@carton_ids.join(',')}"
+          DB[:cartons].where(id: @carton_ids).update(attrs)
+          log_multiple_statuses(:cartons, @carton_ids, 'FIXED CARTON PALLET SEQUENCE IDS', user_name: 'System')
+        end
+      end
+    end
+
+    infodump = <<~STR
+      Script: FixCartonPalletSequence
+
+      What this script does:
+      ----------------------
+      Loop through repacked pallets that has_individual_cartons true and update its cartons.pallet_sequence_id
+
+      Reason for this script:
+      -----------------------
+      update cartons.pallet_sequence_id to repacked pallet_sequence_id if pallet.has_individual_cartons
+
+      Results:
+      --------
+      "#{@carton_ids.count} carton records updated"
+    STR
+
+    unless sequences.nil_or_empty?
+      log_infodump(:data_fix,
+                   :cartons_pallet_sequence_id,
+                   :update_cartons_pallet_sequence_id,
+                   infodump)
+    end
+
+    if debug_mode
+      success_response('Dry run complete')
+    else
+      success_response('Cartons pallet_sequence_id updated successfully')
+    end
+  end
+
+  private
+
+  def repacked_from_pallet_cartons(pallet_id, pallet_sequence_number)  # rubocop:disable Metrics/AbcSize
+    repacked_from_pallet_id = DB[:pallets]
+                              .join(:pallet_sequences, scrapped_from_pallet_id: :id)
+                              .where(Sequel[:pallets][:id] => pallet_id)
+                              .where(:has_individual_cartons)
+                              .get(:repacked_from_pallet_id)
+
+    return if repacked_from_pallet_id.nil_or_empty?
+
+    sequence_id = DB[:pallet_sequences]
+                  .where(scrapped_from_pallet_id: repacked_from_pallet_id)
+                  .where(pallet_sequence_number: pallet_sequence_number)
+                  .get(:id)
+
+    @carton_ids << DB[:cartons].where(pallet_sequence_id: sequence_id).map(:id)
+
+    repacked_from_pallet_cartons(repacked_from_pallet_id, pallet_sequence_number)
+  end
+end
