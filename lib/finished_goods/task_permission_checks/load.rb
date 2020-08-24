182b896d0abdde099d4d578199c651e9c7fe56be
FritzViljoen <fritzviljoen@outlook.com>
2020-08-24 13:26:14 +0200

    fix [Error NSPK-UD] Sequel::DatabaseError: PG::NumericValueOutOfRange: ERROR
    for Path: /rmd/finished_goods/dispatch/truck_arrival/load

--- lib/finished_goods/task_permission_checks/load.rb
+++ lib/finished_goods/task_permission_checks/load.rb
@@ -8,9 +8,9 @@ module FinishedGoodsApp
         @task = task
         @repo = LoadRepo.new
         @id = load_id
-        @pallet_numbers = pallet_numbers || @repo.select_values(:pallets, :pallet_number, load_id: id)
-        return if @id.to_i > AppConst::MAX_DB_INT
+        raise ArgumentError, 'Load id not valid' if @id.to_i > AppConst::MAX_DB_INT
 
+        @pallet_numbers = pallet_numbers || @repo.select_values(:pallets, :pallet_number, load_id: id)
         @entity = @repo.find_load_flat(@id)
       end
 
