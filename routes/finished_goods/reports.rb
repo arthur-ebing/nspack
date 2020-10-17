c17ad46fbed3df18d5f9347dcd93f8a6c0fd24fa
James Silberbauer <jamessil@telkomsa.net>
2020-10-17 07:19:52 +0200

    All finished goods reports set to use Jruby Jasper

--- routes/finished_goods/reports.rb
+++ routes/finished_goods/reports.rb
@@ -7,14 +7,24 @@ class Nspack < Roda # rubocop:disable Metrics/ClassLength
     # DELIVERY NOTE
     # --------------------------------------------------------------------------
     r.on 'dispatch_note', Integer do |id|
-      res = CreateJasperReport.call(report_name: 'dispatch_note',
-                                    user: current_user.login_name,
-                                    file: 'dispatch_note',
-                                    params: { load_id: id,
-                                              pallet_report: 'detail',
-                                              for_picklist: 'false|boolean',
-                                              cartons_equals_pallets: "#{AppConst::CARTON_EQUALS_PALLET}|boolean",
-                                              keep_file: false })
+      res = if AppConst::JASPER_NEW_METHOD
+              jasper_params = JasperParams.new('dispatch_note',
+                                               current_user.login_name,
+                                               load_id: id,
+                                               pallet_report: 'detail',
+                                               for_picklist: false,
+                                               cartons_equals_pallets: AppConst::CARTON_EQUALS_PALLET)
+              CreateJasperReportNew.call(jasper_params)
+            else
+              CreateJasperReport.call(report_name: 'dispatch_note',
+                                      user: current_user.login_name,
+                                      file: 'dispatch_note',
+                                      params: { load_id: id,
+                                                pallet_report: 'detail',
+                                                for_picklist: 'false|boolean',
+                                                cartons_equals_pallets: "#{AppConst::CARTON_EQUALS_PALLET}|boolean",
+                                                keep_file: false })
+            end
       if res.success
         change_window_location_via_json(UtilityFunctions.cache_bust_url(res.instance), request.path)
       else
@@ -25,14 +35,24 @@ class Nspack < Roda # rubocop:disable Metrics/ClassLength
     # DELIVERY NOTE - SUMMARISED
     # --------------------------------------------------------------------------
     r.on 'dispatch_note_summarised', Integer do |id|
-      res = CreateJasperReport.call(report_name: 'dispatch_note',
-                                    user: current_user.login_name,
-                                    file: 'dispatch_note',
-                                    params: { load_id: id,
-                                              pallet_report: 'summary',
-                                              for_picklist: 'false|boolean',
-                                              cartons_equals_pallets: "#{AppConst::CARTON_EQUALS_PALLET}|boolean",
-                                              keep_file: false })
+      res = if AppConst::JASPER_NEW_METHOD
+              jasper_params = JasperParams.new('dispatch_note',
+                                               current_user.login_name,
+                                               load_id: id,
+                                               pallet_report: 'summary',
+                                               for_picklist: false,
+                                               cartons_equals_pallets: AppConst::CARTON_EQUALS_PALLET)
+              CreateJasperReportNew.call(jasper_params)
+            else
+              CreateJasperReport.call(report_name: 'dispatch_note',
+                                      user: current_user.login_name,
+                                      file: 'dispatch_note',
+                                      params: { load_id: id,
+                                                pallet_report: 'summary',
+                                                for_picklist: 'false|boolean',
+                                                cartons_equals_pallets: "#{AppConst::CARTON_EQUALS_PALLET}|boolean",
+                                                keep_file: false })
+            end
       if res.success
         change_window_location_via_json(UtilityFunctions.cache_bust_url(res.instance), request.path)
       else
@@ -44,14 +64,24 @@ class Nspack < Roda # rubocop:disable Metrics/ClassLength
     # --------------------------------------------------------------------------
     r.on 'picklist', Integer do |id|
       report_name = AppConst::USE_EXTENDED_PALLET_PICKLIST ? 'dispatch_picklist' : 'picklist'
-      res = CreateJasperReport.call(report_name: 'dispatch_note',
-                                    user: current_user.login_name,
-                                    file: report_name,
-                                    params: { load_id: id,
-                                              pallet_report: 'detail',
-                                              for_picklist: 'true|boolean',
-                                              cartons_equals_pallets: "#{AppConst::CARTON_EQUALS_PALLET}|boolean",
-                                              keep_file: false })
+      res = if AppConst::JASPER_NEW_METHOD
+              jasper_params = JasperParams.new(report_name,
+                                               current_user.login_name,
+                                               load_id: id,
+                                               pallet_report: 'detail',
+                                               for_picklist: true,
+                                               cartons_equals_pallets: AppConst::CARTON_EQUALS_PALLET)
+              CreateJasperReportNew.call(jasper_params)
+            else
+              CreateJasperReport.call(report_name: 'dispatch_note',
+                                      user: current_user.login_name,
+                                      file: report_name,
+                                      params: { load_id: id,
+                                                pallet_report: 'detail',
+                                                for_picklist: 'true|boolean',
+                                                cartons_equals_pallets: "#{AppConst::CARTON_EQUALS_PALLET}|boolean",
+                                                keep_file: false })
+            end
       if res.success
         change_window_location_via_json(UtilityFunctions.cache_bust_url(res.instance), request.path)
       else
@@ -86,30 +116,45 @@ class Nspack < Roda # rubocop:disable Metrics/ClassLength
       end
     end
 
-    # VERIFIED GROSS MASS
-    # --------------------------------------------------------------------------
-    r.on 'verified_gross_mass', Integer do |id|
-      res = CreateJasperReport.call(report_name: 'container_mass_declaration',
-                                    user: current_user.login_name,
-                                    file: 'container_mass_declaration',
-                                    params: { load_container_id: @repo.get_id(:load_containers, load_id: id),
-                                              keep_file: false })
-      if res.success
-        change_window_location_via_json(UtilityFunctions.cache_bust_url(res.instance), request.path)
-      else
-        show_error(res.message, fetch?(r))
-      end
-    end
+    # # VERIFIED GROSS MASS
+    # # --------------------------------------------------------------------------
+    # r.on 'verified_gross_mass', Integer do |id|
+    #   res = if AppConst::JASPER_NEW_METHOD
+    #           jasper_params = JasperParams.new('container_mass_declaration',
+    #                                            current_user.login_name,
+    #                                            load_container_id: @repo.get_id(:load_containers, load_id: id))
+    #           CreateJasperReportNew.call(jasper_params)
+    #         else
+    #           CreateJasperReport.call(report_name: 'container_mass_declaration',
+    #                                   user: current_user.login_name,
+    #                                   file: 'container_mass_declaration',
+    #                                   params: { load_container_id: @repo.get_id(:load_containers, load_id: id),
+    #                                             keep_file: false })
+    #         end
+    #   if res.success
+    #     change_window_location_via_json(UtilityFunctions.cache_bust_url(res.instance), request.path)
+    #   else
+    #     show_error(res.message, fetch?(r))
+    #   end
+    # end
 
     # VERIFIED GROSS MASS
     # --------------------------------------------------------------------------
     r.on 'verified_gross_mass', Integer do |id|
-      res = CreateJasperReport.call(report_name: 'container_mass_declaration',
-                                    user: current_user.login_name,
-                                    file: 'container_mass_declaration',
-                                    params: { load_container_id: @repo.get_id(:load_containers, load_id: id),
-                                              user_name: current_user.user_name,
-                                              keep_file: false })
+      res = if AppConst::JASPER_NEW_METHOD
+              jasper_params = JasperParams.new('container_mass_declaration',
+                                               current_user.login_name,
+                                               load_container_id: @repo.get_id(:load_containers, load_id: id),
+                                               user_name: current_user.user_name)
+              CreateJasperReportNew.call(jasper_params)
+            else
+              CreateJasperReport.call(report_name: 'container_mass_declaration',
+                                      user: current_user.login_name,
+                                      file: 'container_mass_declaration',
+                                      params: { load_container_id: @repo.get_id(:load_containers, load_id: id),
+                                                user_name: current_user.user_name,
+                                                keep_file: false })
+            end
       if res.success
         change_window_location_via_json(UtilityFunctions.cache_bust_url(res.instance), request.path)
       else
@@ -120,13 +165,22 @@ class Nspack < Roda # rubocop:disable Metrics/ClassLength
     # GOVT INSPECTION SHEET PASSED
     # --------------------------------------------------------------------------
     r.on 'passed_inspection_report', Integer do |id|
-      res = CreateJasperReport.call(report_name: 'govt_inspection_report',
-                                    user: current_user.login_name,
-                                    file: 'govt_inspection_report',
-                                    params: { govt_inspection_sheet_id: id,
-                                              inspection_passed: 'true|boolean',
-                                              signee_caption: AppConst::GOVT_INSPECTION_SIGNEE_CAPTION,
-                                              keep_file: false })
+      res = if AppConst::JASPER_NEW_METHOD
+              jasper_params = JasperParams.new('govt_inspection_report',
+                                               current_user.login_name,
+                                               govt_inspection_sheet_id: id,
+                                               inspection_passed: true,
+                                               signee_caption: AppConst::GOVT_INSPECTION_SIGNEE_CAPTION)
+              CreateJasperReportNew.call(jasper_params)
+            else
+              CreateJasperReport.call(report_name: 'govt_inspection_report',
+                                      user: current_user.login_name,
+                                      file: 'govt_inspection_report',
+                                      params: { govt_inspection_sheet_id: id,
+                                                inspection_passed: 'true|boolean',
+                                                signee_caption: AppConst::GOVT_INSPECTION_SIGNEE_CAPTION,
+                                                keep_file: false })
+            end
       if res.success
         change_window_location_via_json(UtilityFunctions.cache_bust_url(res.instance), request.path)
       else
@@ -137,13 +191,22 @@ class Nspack < Roda # rubocop:disable Metrics/ClassLength
     # GOVT INSPECTION SHEET FAILED
     # --------------------------------------------------------------------------
     r.on 'failed_inspection_report', Integer do |id|
-      res = CreateJasperReport.call(report_name: 'govt_inspection_report',
-                                    user: current_user.login_name,
-                                    file: 'govt_inspection_report',
-                                    params: { govt_inspection_sheet_id: id,
-                                              inspection_passed: 'false|boolean',
-                                              signee_caption: AppConst::GOVT_INSPECTION_SIGNEE_CAPTION,
-                                              keep_file: false })
+      res = if AppConst::JASPER_NEW_METHOD
+              jasper_params = JasperParams.new('govt_inspection_report',
+                                               current_user.login_name,
+                                               govt_inspection_sheet_id: id,
+                                               inspection_passed: false,
+                                               signee_caption: AppConst::GOVT_INSPECTION_SIGNEE_CAPTION)
+              CreateJasperReportNew.call(jasper_params)
+            else
+              CreateJasperReport.call(report_name: 'govt_inspection_report',
+                                      user: current_user.login_name,
+                                      file: 'govt_inspection_report',
+                                      params: { govt_inspection_sheet_id: id,
+                                                inspection_passed: 'false|boolean',
+                                                signee_caption: AppConst::GOVT_INSPECTION_SIGNEE_CAPTION,
+                                                keep_file: false })
+            end
       if res.success
         change_window_location_via_json(UtilityFunctions.cache_bust_url(res.instance), request.path)
       else
@@ -154,12 +217,20 @@ class Nspack < Roda # rubocop:disable Metrics/ClassLength
     # GOVT FINDING SHEET
     # --------------------------------------------------------------------------
     r.on 'finding_sheet', Integer do |id|
-      res = CreateJasperReport.call(report_name: 'govt_finding_sheet',
-                                    user: current_user.login_name,
-                                    file: 'govt_finding_sheet',
-                                    parent_folder: AppConst::RPT_INDUSTRY,
-                                    params: { govt_inspection_sheet_id: id,
-                                              keep_file: false })
+      res = if AppConst::JASPER_NEW_METHOD
+              jasper_params = JasperParams.new('govt_finding_sheet',
+                                               current_user.login_name,
+                                               govt_inspection_sheet_id: id)
+              jasper_params.parent_folder = AppConst::RPT_INDUSTRY
+              CreateJasperReportNew.call(jasper_params)
+            else
+              CreateJasperReport.call(report_name: 'govt_finding_sheet',
+                                      user: current_user.login_name,
+                                      file: 'govt_finding_sheet',
+                                      parent_folder: AppConst::RPT_INDUSTRY,
+                                      params: { govt_inspection_sheet_id: id,
+                                                keep_file: false })
+            end
       if res.success
         change_window_location_via_json(UtilityFunctions.cache_bust_url(res.instance), request.path)
       else
@@ -171,11 +242,19 @@ class Nspack < Roda # rubocop:disable Metrics/ClassLength
     # --------------------------------------------------------------------------
     r.on 'print_tripsheet', Integer do |id|
       vehicle_job_id = FinishedGoodsApp::GovtInspectionRepo.new.get_id(:vehicle_jobs, govt_inspection_sheet_id: id)
-      res = CreateJasperReport.call(report_name: 'interwarehouse',
-                                    user: current_user.login_name,
-                                    file: 'interwarehouse',
-                                    params: { vehicle_job_id: vehicle_job_id,
-                                              keep_file: false })
+      res = if AppConst::JASPER_NEW_METHOD
+              jasper_params = JasperParams.new('interwarehouse',
+                                               current_user.login_name,
+                                               vehicle_job_id: vehicle_job_id)
+              jasper_params.parent_folder = AppConst::RPT_INDUSTRY
+              CreateJasperReportNew.call(jasper_params)
+            else
+              CreateJasperReport.call(report_name: 'interwarehouse',
+                                      user: current_user.login_name,
+                                      file: 'interwarehouse',
+                                      params: { vehicle_job_id: vehicle_job_id,
+                                                keep_file: false })
+            end
       if res.success
         change_window_location_via_json(UtilityFunctions.cache_bust_url(res.instance), request.path)
       else
