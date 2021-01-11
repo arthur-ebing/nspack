0d32904decab03851c6181cf6fdcf60a82653b99
James Silberbauer <jamessil@telkomsa.net>
2021-01-11 11:28:39 +0200

    Group incentive gets external system boolean

--- db/migrations/202101111106_add_from_ext_sys_to_group_incentive.rb
+++ db/migrations/202101111106_add_from_ext_sys_to_group_incentive.rb
@@ -1,13 +1,21 @@
 Sequel.migration do
   up do
-    alter_table(:group_incentives) do
-      add_column :from_external_system, :boolean, default: false
+    if ENV['CLIENT_CODE'] != 'kr'
+      puts 'Migration not run (only applicable to Kromco)'
+    else
+      alter_table(:group_incentives) do
+        add_column :from_external_system, :boolean, default: false
+      end
     end
   end
 
   down do
-    alter_table(:group_incentives) do
-      drop_column :from_external_system
+    if ENV['CLIENT_CODE'] != 'kr'
+      puts 'Migration not run (only applicable to Kromco)'
+    else
+      alter_table(:group_incentives) do
+        drop_column :from_external_system
+      end
     end
   end
 end
