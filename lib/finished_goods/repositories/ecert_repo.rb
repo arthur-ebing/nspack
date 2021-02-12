ed0d1e11dfba1ae68a90fd3cd708e2c81ecc8e6d
FritzViljoen <fritzviljoen@outlook.com>
2021-02-12 18:05:50 +0200

    EcertRepo move magic methods to top of page

--- lib/finished_goods/repositories/ecert_repo.rb
+++ lib/finished_goods/repositories/ecert_repo.rb
@@ -2,8 +2,14 @@
 
 module FinishedGoodsApp
   class EcertRepo < BaseRepo
-    build_for_select :ecert_agreements, label: %i[code name], value: :id, order_by: :code
-    build_inactive_select :ecert_agreements, label: %i[code name], value: :id, order_by: :code
+    build_for_select :ecert_agreements,
+                     label: %i[code name],
+                     value: :id,
+                     order_by: :code
+    build_inactive_select :ecert_agreements,
+                          label: %i[code name],
+                          value: :id,
+                          order_by: :code
     crud_calls_for :ecert_agreements, name: :ecert_agreement, wrapper: EcertAgreement
 
     crud_calls_for :ecert_tracking_units, name: :ecert_tracking_unit
