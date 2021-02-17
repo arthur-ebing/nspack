8adc9bfb18ad89e1f0c57361ce9a4081f2b0f573
miramanz <miraclemanzongo@gmail,com>
2021-02-17 17:09:58 +0200

    Print carton_labels from Production | In progress | Active Setups

--- routes/production/in_progress.rb
+++ routes/production/in_progress.rb
@@ -14,13 +14,15 @@ class Nspack < Roda
             # select running runs that are labeling by showing their line numbers
             show_partial_or_page(r) { Production::Runs::ProductionRun::SelectLabelLine.call }
           else
-            r.redirect "/list/product_setups_on_runs/with_params?key=standard&production_run_id=#{run_id}"
+            grid = AppConst::CR_PROD.use_packing_specifications? ? 'packing_specification_items_on_runs' : 'product_setups_on_runs'
+            r.redirect "/list/#{grid}/with_params?key=standard&production_run_id=#{run_id}"
           end
         end
 
         r.post do
           run_id = params[:production_run][:production_run_id]
-          r.redirect "/list/product_setups_on_runs/with_params?key=standard&production_run_id=#{run_id}"
+          grid = AppConst::CR_PROD.use_packing_specifications? ? 'packing_specification_items_on_runs' : 'product_setups_on_runs'
+          r.redirect "/list/#{grid}/with_params?key=standard&production_run_id=#{run_id}"
         end
       end
     end
