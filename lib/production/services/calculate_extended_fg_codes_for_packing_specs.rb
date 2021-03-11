289c60029149a70cf3c75e84926f78d8298e308c
FritzViljoen <fritzviljoen@outlook.com>
2021-03-11 18:47:54 +0200

    fix error [Error KRNSPK] JSON::ParserError: 809: unexpected token at ''

--- lib/production/services/calculate_extended_fg_codes_for_packing_specs.rb
+++ lib/production/services/calculate_extended_fg_codes_for_packing_specs.rb
@@ -18,19 +18,22 @@ module ProductionApp
 
     def calculate_extended_fg_codes
       extended_fg_code = ProductionApp::ProductSetupRepo.new.calculate_extended_fg_code(packing_specification_item_id)
-      extended_fg_id = extended_fg_id(extended_fg_code)
+      extended_fg_id = request_extended_fg_id(extended_fg_code)
 
       args = { legacy_data: { extended_fg_code: extended_fg_code, extended_fg_id:  extended_fg_id } }
       ProductionApp::PackingSpecificationRepo.new.update_packing_specification_item(packing_specification_item_id, args)
     end
 
-    def extended_fg_id(extended_fg_code)
+    def request_extended_fg_id(extended_fg_code)
       url = "#{AppConst::RMT_INTEGRATION_SERVER_URI}/services/integration/get_extended_fg?extended_fg_code=#{extended_fg_code}"
       http = Crossbeams::HTTPCalls.new
       res = http.request_get(url)
       return res.message unless res.success
 
-      JSON.parse(res.instance.body)
+      instance = res.instance.body
+      return 'Nothing returned from MES' if instance.nil_or_empty?
+
+      JSON.parse(instance)
     end
   end
 end
