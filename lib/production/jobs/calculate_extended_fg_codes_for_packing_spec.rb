4b63e9467ab87875f76d5bc3806483916ad64911
FritzViljoen <fritzviljoen@outlook.com>
2021-03-12 12:16:03 +0200

    fix job call

--- lib/production/jobs/calculate_extended_fg_codes_for_packing_spec.rb
+++ lib/production/jobs/calculate_extended_fg_codes_for_packing_spec.rb
@@ -4,7 +4,7 @@ module ProductionApp
   module Job
     class CalculateExtendedFgCodesForPackingSpecs < BaseQueJob
       def run(packing_specification_item_id)
-        res = FinishedGoodsApp::CalculateExtendedFgCodesForPackingSpecs.call(packing_specification_item_id)
+        res = ProductionApp::CalculateExtendedFgCodesForPackingSpecs.call(packing_specification_item_id)
         raise res.message unless res.success
 
         finish
