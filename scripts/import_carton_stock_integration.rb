bf15f06b5fae4ce0e47f807376c19a245c61dec6
FritzViljoen <fritzviljoen@outlook.com>
2021-04-21 13:31:39 +0200

    fix update import_carton_stock_integration rename production_run_id to legacy_production_run_id

--- scripts/import_carton_stock_integration.rb
+++ scripts/import_carton_stock_integration.rb
@@ -69,7 +69,7 @@ class ImportCartonStockIntegration < BaseScript # rubocop:disable Metrics/ClassL
       params[:pallet_sequence_id] = create_pallet_sequence(params)
       carton_numbers = params[:carton_numbers].split('|')
       carton_numbers.each do |carton_number|
-        params[legacy_carton_number: carton_number]
+        params[:legacy_carton_number] = carton_number
 
         params[:carton_label_id] = create_carton_label(params)
         create_carton(params)
