4d1e6a1714ae10a110ad7c95c9c52180e8e50bd6
FritzViljoen <fritzviljoen@outlook.com>
2021-05-12 14:55:26 +0200

    update max_pallets_on_load client setting to client_fg_rules use max_bins for fg.loads where rmt_loads

--- lib/client_rules/client_fg_rules.rb
+++ lib/client_rules/client_fg_rules.rb
@@ -11,44 +11,30 @@ module Crossbeams
       hb: { place_of_issue_for_addendum: 'PLZ',
             vgm_required: false,
             integrate_extended_fg: false,
-            max_pallets_on_load: 50,
-            max_rmt_bins_on_load: 50,
             valid_pallet_destination: { failed: [/.+/], pending: [/.+/], loaded: [/.+/] } },
       hl: { place_of_issue_for_addendum: 'PLZ',
             vgm_required: false,
             integrate_extended_fg: false,
-            max_pallets_on_load: 50,
-            max_rmt_bins_on_load: 50,
             valid_pallet_destination: { failed: [/.+/], pending: [/.+/], loaded: [/.+/] } },
       kr: { place_of_issue_for_addendum: 'CPT',
             vgm_required: true,
             integrate_extended_fg: true,
-            max_pallets_on_load: 50,
-            max_rmt_bins_on_load: 50,
             valid_pallet_destination: { failed: [/\AREWORKS$/], pending: [/\AREWORKS$/, /\ARA_10/, /\APACKHSE/], loaded: [/\APART_PALLETS/] } },
       um: { place_of_issue_for_addendum: nil,
             vgm_required: true,
             integrate_extended_fg: false,
-            max_pallets_on_load: 50,
-            max_rmt_bins_on_load: 50,
             valid_pallet_destination: { failed: [/.+/], pending: [/.+/], loaded: [/.+/] } },
       ud: { place_of_issue_for_addendum: 'PLZ',
             vgm_required: true,
             integrate_extended_fg: false,
-            max_pallets_on_load: 50,
-            max_rmt_bins_on_load: 50,
             valid_pallet_destination: { failed: [/.+/], pending: [/.+/], loaded: [/.+/] } },
       sr: { place_of_issue_for_addendum: 'PLZ',
             vgm_required: true,
             integrate_extended_fg: false,
-            max_pallets_on_load: 50,
-            max_rmt_bins_on_load: 50,
             valid_pallet_destination: { failed: [/.+/], pending: [/.+/], loaded: [/.+/] } },
       sr2: { place_of_issue_for_addendum: 'PLZ',
              vgm_required: true,
              integrate_extended_fg: false,
-             max_pallets_on_load: 50,
-             max_rmt_bins_on_load: 50,
              valid_pallet_destination: { failed: [/.+/], pending: [/.+/], loaded: [/.+/] } }
     }.freeze
     # ALLOW_EXPORT_PALLETS_TO_BYPASS_INSPECTION
@@ -120,13 +106,13 @@ module Crossbeams
     def max_pallet_count_for_load?(explain: false)
       return 'Limits the amount of pallets that can be loaded.' if explain
 
-      setting(:max_pallets_on_load)
+      setting(:max_pallets_on_load) || 50
     end
 
     def max_bin_count_for_load?(explain: false)
       return 'Limits the amount of RMT Bins that can be loaded.' if explain
 
-      setting(:max_rmt_bins_on_load)
+      setting(:max_rmt_bins_on_load) || 50
     end
   end
 end
