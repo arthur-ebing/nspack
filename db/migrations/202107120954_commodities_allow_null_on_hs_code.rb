83cfecc30fe02ad321cdc849fb4d90d75e60570e
FritzViljoen <fritzviljoen@outlook.com>
2021-07-13 09:30:08 +0200

    migration: fix rollback

--- db/migrations/202107120954_commodities_allow_null_on_hs_code.rb
+++ db/migrations/202107120954_commodities_allow_null_on_hs_code.rb
@@ -5,6 +5,8 @@ Sequel.migration do
     end
   end
   down do
+    run "UPDATE commodities SET hs_code = 'TEXT' WHERE hs_code IS NULL;"
+
     alter_table(:commodities) do
       set_column_not_null :hs_code
     end
