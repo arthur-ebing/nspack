19e81bd2599b5e78c13685073f5a44c6af76504e
Miracle <miraclemanzongo@gmail.com>
2021-07-22 14:09:04 +0200

    clone run -  copy packing_specification_item_id,target_customer_party_role_id and work_order_item_id to new run's product_resource_allocations (#15)
    Co-authored-by: miramanz <miraclemanzongo@gmail,com>

--- lib/production/repositories/production_run_repo.rb
+++ lib/production/repositories/production_run_repo.rb
@@ -168,8 +168,11 @@ module ProductionApp
       attrs = find_hash(:production_runs, id).reject { |k, _| ignore_cols.include?(k) }
       new_id = create_production_run(attrs.merge(cloned_from_run_id: id))
       clone_alloc = <<~SQL
-        INSERT INTO product_resource_allocations(production_run_id, plant_resource_id, product_setup_id, label_template_id, packing_method_id)
-        SELECT #{new_id}, plant_resource_id, product_setup_id, label_template_id, packing_method_id
+        INSERT INTO product_resource_allocations(production_run_id, plant_resource_id, product_setup_id,
+                    label_template_id, packing_method_id, packing_specification_item_id, target_customer_party_role_id,
+                    work_order_item_id)
+        SELECT #{new_id}, plant_resource_id, product_setup_id, label_template_id, packing_method_id, packing_specification_item_id,
+               target_customer_party_role_id, work_order_item_id
         FROM product_resource_allocations
         WHERE production_run_id = ?
       SQL
