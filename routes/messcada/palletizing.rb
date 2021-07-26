c323f8270a92ce1a378ffc83aac336a9d1641694
James Silberbauer <jamessil@telkomsa.net>
2021-07-26 11:03:29 +0200

    Rescue invalid param at carton palletizing

--- routes/messcada/palletizing.rb
+++ routes/messcada/palletizing.rb
@@ -121,5 +121,15 @@ class Nspack < Roda
       feedback = interactor.palletizing_robot_feedback(params[:device], res)
       Crossbeams::RobotResponder.new(feedback).render
     end
+
+  rescue Rack::QueryParser::InvalidParameterError => e
+    ErrorMailer.send_exception_email(e,
+                                     subject: 'Carton palletizing invalid parameter',
+                                     message: "Invalid param from route: #{request.path} with #{request.query_string}.\nFrom ip: #{request.ip}")
+
+    feedback = MesscadaApp::RobotFeedback.new(device: '',
+                                              status: false,
+                                              line1: 'Unable to read barcode')
+    Crossbeams::RobotResponder.new(feedback).render
   end
 end
