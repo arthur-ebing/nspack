1b06ecdd830adefd455b9cf2c9545f22e9dd9f11
user <lux@nosoft.biz>
2021-07-27 12:55:35 +0200

    presorting - latest

---
+++ lib/raw_materials/validations/presort_staging_run_child_schema.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module RawMaterialsApp
+  PresortStagingRunChildSchema = Dry::Schema.Params do
+    optional(:id).filled(:integer)
+    required(:presort_staging_run_id).maybe(:integer)
+    optional(:completed_at).maybe(:time)
+    optional(:staged_at).maybe(:time)
+    optional(:canceled).maybe(:bool)
+    required(:farm_id).filled(:integer)
+    optional(:editing).maybe(:bool)
+    optional(:staged).maybe(:bool)
+  end
+end
