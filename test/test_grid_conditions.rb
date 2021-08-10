23a3cfcd24ed243fc8aa016110dbdc09b0688f35
James Silberbauer <jamessil@telkomsa.net>
2021-08-10 06:54:50 +0200

    Test list and search files - check for invalid action attributes

--- test/test_grid_conditions.rb
+++ test/test_grid_conditions.rb
@@ -22,11 +22,13 @@ class TestGridQueries < MiniTestWithHooks
   def test_list_grids
     load_all_queries
     errs = []
+    acts = []
     lists = Dir.glob('grid_definitions/lists/*.yml')
     lists.each do |file|
       hash = YAML.load(File.read(file))
 
       conditions = hash[:conditions]
+      actions = hash[:actions]
       query = hash[:dataminer_definition]
       assert @query_columns[query], %(List grid: "#{File.basename(file).sub(File.extname(file), '')}" references non-existent query "#{query}")
       client_queries = if hash[:dataminer_client_definitions]
@@ -50,12 +52,22 @@ class TestGridQueries < MiniTestWithHooks
           end
         end
       end
+
+      actions&.each do |action|
+        if action[:hide_if_false] && action[:hide_if_false].to_s.include?(', ')
+          acts << %(List: "#{File.basename(file).sub(File.extname(file), '')}", action: "#{action[:text]}" - hide_if_false: "#{action[:hide_if_false]}" contains a space after the comma")
+        end
+        if action[:hide_if_true] && action[:hide_if_true].to_s.include?(', ')
+          acts << %(List: "#{File.basename(file).sub(File.extname(file), '')}", action: "#{action[:text]}" - hide_if_true: "#{action[:hide_if_true]}" contains a space after the comma")
+        end
+      end
     end
 
-    if errs.empty?
+    if errs.empty? && acts.empty?
       assert true
     else
-      assert false, "\n--------\nList yml files with invalid column names for conditions:\n--------\n#{errs.join("\n")}"
+      assert false, "\n--------\nList yml files with invalid column names for conditions:\n--------\n#{errs.join("\n")}" if errs.length.positive?
+      assert false, "\n--------\nList yml files with invalid hide actions:\n--------\n#{acts.join("\n")}" if acts.length.positive?
     end
   end
 
@@ -84,6 +96,31 @@ class TestGridQueries < MiniTestWithHooks
       assert true
     else
       assert false, "\n----------\nLookup yml files with invalid column names for conditions:\n----------\n#{errs.join("\n")}"
+    end
+  end
+
+  # Check that search yml definitions do not have invalid actions
+  def test_search_grids
+    acts = []
+    searches = Dir.glob('grid_definitions/searches/*.yml')
+    searches.each do |file|
+      hash = YAML.load(File.read(file))
+      actions = hash[:actions]
+
+      actions&.each do |action|
+        if action[:hide_if_false] && action[:hide_if_false].to_s.include?(', ')
+          acts << %(Search: "#{File.basename(file).sub(File.extname(file), '')}", action: "#{action[:text]}" - hide_if_false: "#{action[:hide_if_false]}" contains a space after the comma")
+        end
+        if action[:hide_if_true] && action[:hide_if_true].to_s.include?(', ')
+          acts << %(Search: "#{File.basename(file).sub(File.extname(file), '')}", action: "#{action[:text]}" - hide_if_true: "#{action[:hide_if_true]}" contains a space after the comma")
+        end
+      end
+    end
+
+    if acts.empty?
+      assert true
+    else
+      assert false, "\n--------\nSearch yml files with invalid hide actions:\n--------\n#{acts.join("\n")}"
     end
   end
 end
