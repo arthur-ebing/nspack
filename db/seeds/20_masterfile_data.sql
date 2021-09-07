07e4026cb21fe551e0a4623260e317a450bb9b8a
James Silberbauer <jamessil@telkomsa.net>
2021-09-07 15:56:41 +0200

    Fix to emptying pallet location and seeds for SITE

--- db/seeds/20_masterfile_data.sql
+++ db/seeds/20_masterfile_data.sql
@@ -108,23 +108,35 @@ INSERT INTO reworks_run_types (run_type, description) VALUES('SCRAP CARTON', 'Sc
 INSERT INTO reworks_run_types (run_type, description) VALUES('UNSCRAP CARTON', 'Unscrap Carton') ON CONFLICT DO NOTHING;
 
 -- LOCATION TYPES
+INSERT INTO location_types (location_type_code, short_code, hierarchical) VALUES('SITE', 'SITE', 't') ON CONFLICT DO NOTHING;
 INSERT INTO location_types (location_type_code, short_code, hierarchical) VALUES('BIN_ASSET', 'BIN_ASSET', 'f') ON CONFLICT DO NOTHING;
 INSERT INTO location_types (location_type_code, short_code, hierarchical) VALUES('FARM', 'FARM', 'f') ON CONFLICT DO NOTHING;
+INSERT INTO location_types (location_type_code, short_code, hierarchical) VALUES('PENDING_LOCATION', 'PENDING_LOCATION', 't') ON CONFLICT DO NOTHING;
+INSERT INTO location_types (location_type_code, short_code, hierarchical) VALUES('IN_TRANSIT', 'IN_TRANSIT', 'f') ON CONFLICT DO NOTHING;
 
 -- LOCATION STORAGE TYPES
+INSERT INTO location_storage_types (storage_type_code) VALUES('SITE') ON CONFLICT DO NOTHING;
 INSERT INTO location_storage_types (storage_type_code) VALUES('PALLETS') ON CONFLICT DO NOTHING;
 INSERT INTO location_storage_types (storage_type_code) VALUES('RMT_PALLETS') ON CONFLICT DO NOTHING;
 INSERT INTO location_storage_types (storage_type_code) VALUES('BIN_ASSET') ON CONFLICT DO NOTHING;
+INSERT INTO location_storage_types (storage_type_code) VALUES('PALLETS') ON CONFLICT DO NOTHING;
+
+-- LOCATION ASSIGNMENTS
+INSERT INTO location_assignments (assignment_code) VALUES('SITE') ON CONFLICT DO NOTHING;
+INSERT INTO location_assignments (assignment_code) VALUES('TRANSIT') ON CONFLICT DO NOTHING;
 
 -- IN-TRANSIT LOCATION (Not part of locations tree)
-INSERT INTO location_types (location_type_code, short_code, hierarchical) VALUES('IN_TRANSIT', 'IN_TRANSIT', 'f') ON CONFLICT DO NOTHING;
-INSERT INTO location_storage_types (storage_type_code) VALUES('PALLETS') ON CONFLICT DO NOTHING;
-INSERT INTO location_assignments (assignment_code) VALUES('TRANSIT') ON CONFLICT DO NOTHING;
 INSERT INTO locations (primary_storage_type_id, location_type_id, primary_assignment_id, location_long_code, location_description, location_short_code, can_be_moved, can_store_stock)
 VALUES ((SELECT id FROM location_storage_types WHERE storage_type_code = 'PALLETS'), (SELECT id FROM location_types WHERE location_type_code = 'IN_TRANSIT'), (SELECT id FROM location_assignments WHERE assignment_code = 'TRANSIT'), 'IN_TRANSIT_EX_PACKHSE', 'IN_TRANSIT_EX_PACKHSE', 'IN_TRANSIT_EX_PACKHSE', true, true) ON CONFLICT DO NOTHING;
 
+-- SITE LOCATION
+INSERT INTO locations (primary_storage_type_id, location_type_id, primary_assignment_id, location_long_code, location_description, location_short_code, can_store_stock)
+VALUES ((SELECT id FROM location_storage_types WHERE storage_type_code = 'SITE'), (SELECT id FROM location_types WHERE location_type_code = 'SITE'),
+(SELECT id FROM location_assignments WHERE assignment_code = 'SITE'), 'SITE', 'SITE', 'SITE', true) ON CONFLICT DO NOTHING;
+INSERT INTO public.tree_locations(ancestor_location_id, descendant_location_id, path_length)
+VALUES ((SELECT id FROM locations where location_short_code = 'SITE'), (SELECT id FROM locations where location_short_code = 'SITE'), 0) ON CONFLICT DO NOTHING;
+
 -- PENDING LOCATION
-INSERT INTO location_types (location_type_code, short_code, hierarchical) VALUES('PENDING_LOCATION', 'PENDING_LOCATION', 't') ON CONFLICT DO NOTHING;
 INSERT INTO location_assignments (assignment_code) VALUES('STORAGE_AT_UNKNOWN_LOCATION') ON CONFLICT DO NOTHING;
 INSERT INTO locations (primary_storage_type_id, location_type_id, primary_assignment_id, location_long_code, location_description, location_short_code, can_store_stock)
 VALUES ((SELECT id FROM location_storage_types WHERE storage_type_code = 'PALLETS'), (SELECT id FROM location_types WHERE location_type_code = 'PENDING_LOCATION'),
