8a0fd1f212c4f478fe5c1ff3995e02fcf1c81f1e
lmatoti <lmatoti@gmail.com>
2021-09-08 15:51:53 +0200

    Pallet Buildups (#56)
    * Pallet Buildups
    Show pallet_buildups to work for integer carton_numbers
    
    * Code review changes
    
    * Code review changes
    
    Co-authored-by: user <lux@nosoft.biz>

--- lib/finished_goods/views/pallet_buildup/show.rb
+++ lib/finished_goods/views/pallet_buildup/show.rb
@@ -10,21 +10,13 @@ module FinishedGoods
         layout = Crossbeams::Layout::Page.build(rules) do |page|
           page.form_object ui_rule.form_object
           page.form do |form|
-            # form.caption 'Pallet Buildup'
             form.view_only!
-            # form.add_field :qty_cartons_to_move
             form.add_field :created_by
             form.add_field :completed_at
-            # form.add_field :source_pallets
-            # form.add_field :cartons_moved
-            # form.add_field :completed
             form.add_field :destination_pallet_number
 
             ui_rule.form_object[:source_pallets].each do |s|
-              form.add_text "Ctns Moved From Pallet: #{s}", css_classes: 'b mid-gray'
-              ui_rule.form_object[:cartons_moved][s].to_a.each do |c|
-                form.add_field c.to_sym
-              end
+              form.add_list ui_rule.form_object[:cartons_moved][s], caption: "Ctns Moved From Pallet: #{s}"
             end
           end
         end
