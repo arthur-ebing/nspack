328636711483ce775ea0468cdb019df3819d9acc
James Silberbauer <jamessil@telkomsa.net>
2021-09-08 16:40:45 +0200

    Add rebin weight to runs summary DM report

--- config/deploy/ui_matrooz.rb
+++ config/deploy/ui_matrooz.rb
@@ -1,4 +1,4 @@
 # VM instance at Unifrutti Matroozefontein
-server '192.168.100.241', user: 'nspack', roles: %w[app db web]
+server '172.18.0.20', user: 'nspack', roles: %w[app db web]
 set :deploy_to, '/home/nspack/nspack'
 set :chruby_ruby, 'ruby-2.5.5'
