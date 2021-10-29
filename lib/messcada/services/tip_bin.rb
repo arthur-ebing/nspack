f2b94df286b8e9bcd9e104e0710a432cdc39a75a
James Silberbauer <jamessil@telkomsa.net>
2021-10-29 15:34:32 +0200

    Remove call to old MES

--- lib/messcada/services/tip_bin.rb
+++ lib/messcada/services/tip_bin.rb
@@ -31,15 +31,15 @@ module MesscadaApp
       success_response('RMT Bin tipped successfully', @run_attrs.merge(rmt_bin_id: @rmt_bin_id, run_id: @run_id, bins_tipped: run_stats_bins_tipped))
     end
 
-    def can_tip_bin? # rubocop:disable Metrics/AbcSize
+    def can_tip_bin?
       run_res = active_run_for_device
       return run_res unless run_res.success
 
-      is_kr_bin = AppConst::CR_PROD.kromco_rmt_integration?
-      if is_kr_bin
-        res = BinIntegration.new(bin_number, run_res.instance).valid_bin_for_kromco_rmt_system?
-        return res unless res.success
-      end
+      # is_kr_bin = AppConst::CR_PROD.kromco_rmt_integration?
+      # if is_kr_bin
+      #   res = BinIntegration.new(bin_number, run_res.instance).valid_bin_for_kromco_rmt_system?
+      #   return res unless res.success
+      # end
 
       errors = validations
       return failed_response(errors) unless errors.nil?
