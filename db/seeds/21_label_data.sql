587f1c581ba9df236de2a30a286c5572d7a3a2b7
James Silberbauer <jamessil@telkomsa.net>
2021-11-02 19:37:04 +0200

    Add columns to Rebin label view

--- db/seeds/21_label_data.sql
+++ db/seeds/21_label_data.sql
@@ -1,4 +1,5 @@
 -- MASTER LISTS
 INSERT INTO master_lists (list_type, description) VALUES('label_type', 'Carton');
 INSERT INTO master_lists (list_type, description) VALUES('label_type', 'Bin');
+INSERT INTO master_lists (list_type, description) VALUES('label_type', 'Rebin');
 INSERT INTO master_lists (list_type, description) VALUES('label_type', 'Pallet');
