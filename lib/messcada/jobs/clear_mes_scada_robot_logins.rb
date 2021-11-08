6901aa6534c5b9b14506cb3aaa34ad041fd3e2d7
James Silberbauer <jamessil@telkomsa.net>
2021-11-08 21:41:43 +0200

    Robot auth log clearing added to clearLogins job

--- lib/messcada/jobs/clear_mes_scada_robot_logins.rb
+++ lib/messcada/jobs/clear_mes_scada_robot_logins.rb
@@ -10,11 +10,13 @@ module MesscadaApp
         sock = TCPSocket.open(ip_address, 2071)
         request = '<LogoffMes PID="435" />'
         puts "CLEAR MESSCADA LOGINS AT #{system_resource_code} (#{ip_address})"
+        AppConst.log_authentication("TCP clear messcada logins #{system_resource_code} (#{ip_address}) - #{request}")
 
         sock.puts request
         result = sock.read
 
         puts "RESPONSE FROM #{system_resource_code} : #{result}"
+        AppConst.log_authentication("TCP clear messcada logins - #{result}")
         sock.close
         finish
       end
