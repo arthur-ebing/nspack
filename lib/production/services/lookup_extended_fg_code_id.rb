957d392f972eefa90b9e2d3b16cec32163286273
James Silberbauer <jamessil@telkomsa.net>
2021-11-10 12:29:17 +0200

    Fix returned response from FG lookup

--- lib/production/services/lookup_extended_fg_code_id.rb
+++ lib/production/services/lookup_extended_fg_code_id.rb
@@ -9,27 +9,15 @@ module ProductionApp
     end
 
     def call
-      id = request_extended_fg_id
-
-      if id
-        success_response('ok', id)
-      else
-        failed_response('Not found')
-      end
-    end
-
-    private
-
-    def request_extended_fg_id
       url = "#{AppConst::RMT_INTEGRATION_SERVER_URI}/services/integration/get_extended_fg?extended_fg_code=#{extended_fg_code}"
       http = Crossbeams::HTTPCalls.new
       res = http.request_get(url)
-      return res.message unless res.success
+      return failed_response(res.message) unless res.success
 
       instance = res.instance.body
-      return 'Nothing returned from MES' if instance.nil_or_empty?
+      return failed_response('Nothing returned from MES') if instance.nil_or_empty?
 
-      JSON.parse(instance)
+      success_response('ok', JSON.parse(instance))
     end
   end
 end
