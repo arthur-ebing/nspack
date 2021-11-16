88ef0859f5fe67f684c7a9f89c1ea05004324855
James Silberbauer <jamessil@telkomsa.net>
2021-11-16 14:50:24 +0200

    Fix migration to handle null in bin asset queue created_at col

--- db/migrations/202111161123_add_created_at_to_bin_asset_que.rb
+++ db/migrations/202111161123_add_created_at_to_bin_asset_que.rb
@@ -2,12 +2,18 @@ require 'sequel_postgresql_triggers'
 Sequel.migration do
   up do
     alter_table(:bin_asset_transactions_queue) do
-      add_column :created_at, DateTime, null: false
+      add_column :created_at, DateTime
     end
     pgt_created_at(:bin_asset_transactions_queue,
                    :created_at,
                    function_name: :bin_asset_transactions_queue_set_created_at,
                    trigger_name: :set_created_at)
+
+    run "UPDATE bin_asset_transactions_queue SET created_at = current_timestamp;"
+
+    alter_table(:bin_asset_transactions_queue) do
+      set_column_not_null :created_at
+    end
   end
 
   down do
