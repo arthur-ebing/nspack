15915f64fa52c387047d2fe59395bae0315ae52b
James Silberbauer <jamessil@telkomsa.net>
2022-01-11 16:44:22 +0200

    Staging: include link for Loftus env

--- config/deploy/nosoftvm54.rb
+++ config/deploy/nosoftvm54.rb
@@ -3,4 +3,4 @@ server '192.168.50.54', user: 'nspack', roles: %w[app db web]
 set :deploy_to, '/home/nspack/nspack'
 set :branch, 'develop'
 set :chruby_ruby, 'ruby-2.5.5'
-append :linked_files, '.env.cfg', '.env.kr'
+append :linked_files, '.env.cfg', '.env.kr', '.env.hbl'
