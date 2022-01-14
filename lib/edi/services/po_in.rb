f105fa0993f520493d96859bc045e2e2f63bb50a
James Silberbauer <jamessil@telkomsa.net>
2022-01-14 14:55:36 +0200

    PO missing masterfiles - wrap codes in quotes so that spaces are shown

--- lib/edi/services/po_in.rb
+++ lib/edi/services/po_in.rb
@@ -169,21 +169,21 @@ module EdiApp
 
       standard_pack_code_id = po_repo.find_standard_pack_id(seq[:pack])
       rec[:lookup_data][:standard_pack_code_id] = standard_pack_code_id
-      rec[:missing_mf][:standard_pack_code_id] = { mode: :direct, raise: false, keys: { pack: seq[:pack] }, msg: "Standard Pack: #{seq[:pack]}" } if standard_pack_code_id.nil?
+      rec[:missing_mf][:standard_pack_code_id] = { mode: :direct, raise: false, keys: { pack: seq[:pack] }, msg: "Standard Pack: '#{seq[:pack]}'" } if standard_pack_code_id.nil?
 
       fruit_size_reference_id = po_repo.find_fruit_size_reference_id(seq[:size_count])
       rec[:lookup_data][:fruit_size_reference_id] = fruit_size_reference_id
-      rec[:missing_mf][:fruit_size_reference_id] = { mode: :direct, raise: false, keys: { size_count: seq[:size_count] }, msg: "Size reference: #{seq[:size_count]}" } if fruit_size_reference_id.nil?
+      rec[:missing_mf][:fruit_size_reference_id] = { mode: :direct, raise: false, keys: { size_count: seq[:size_count] }, msg: "Size reference: '#{seq[:size_count]}'" } if fruit_size_reference_id.nil?
 
       basic_pack_code_id = po_repo.find_basic_pack_id(standard_pack_code_id)
       rec[:lookup_data][:basic_pack_code_id] = basic_pack_code_id
-      rec[:missing_mf][:basic_pack_code_id] = { mode: :direct, raise: false, keys: { pack: seq[:pack] }, msg: "Basic pack: #{seq[:pack]}" } if basic_pack_code_id.nil?
+      rec[:missing_mf][:basic_pack_code_id] = { mode: :direct, raise: false, keys: { pack: seq[:pack] }, msg: "Basic pack: '#{seq[:pack]}'" } if basic_pack_code_id.nil?
 
       pallet_format_id, cartons_per_pallet_id = po_repo.find_pallet_format_and_cpp_id(seq[:pallet_btype], tot_cartons, basic_pack_code_id)
       rec[:lookup_data][:pallet_format_id] = pallet_format_id
-      rec[:missing_mf][:pallet_format_id] = { mode: :direct, raise: true, keys: { pallet_btype: seq[:pallet_btype], cartons: tot_cartons, basic_pack_code_id: basic_pack_code_id }, msg: "Pallet format for pallet base: #{seq[:pallet_btype]}"  } if pallet_format_id.nil?
+      rec[:missing_mf][:pallet_format_id] = { mode: :direct, raise: true, keys: { pallet_btype: seq[:pallet_btype], cartons: tot_cartons, basic_pack_code_id: basic_pack_code_id }, msg: "Pallet format for pallet base: '#{seq[:pallet_btype]}'"  } if pallet_format_id.nil?
       rec[:lookup_data][:cartons_per_pallet_id] = cartons_per_pallet_id
-      rec[:missing_mf][:cartons_per_pallet_id] = { mode: :direct, raise: true, keys: { pallet_btype: seq[:pallet_btype], cartons: tot_cartons, basic_pack_code_id: basic_pack_code_id }, msg: "Cartons Per Pallet for pallet #{pallet_number}, pallet base: #{seq[:pallet_btype]}, pack: #{seq[:pack]} and CCP: #{tot_cartons}" } if cartons_per_pallet_id.nil?
+      rec[:missing_mf][:cartons_per_pallet_id] = { mode: :direct, raise: true, keys: { pallet_btype: seq[:pallet_btype], cartons: tot_cartons, basic_pack_code_id: basic_pack_code_id }, msg: "Cartons Per Pallet for pallet '#{pallet_number}', pallet base: '#{seq[:pallet_btype]}', pack: '#{seq[:pack]}' and CCP: '#{tot_cartons}'" } if cartons_per_pallet_id.nil?
 
       gross_weight = if AppConst::CR_PROD.derive_nett_weight?
                        nil
@@ -233,50 +233,50 @@ module EdiApp
 
       puc_id = po_repo.find_puc_id(seq[:farm])
       rec[:lookup_data][:puc_id] = puc_id
-      rec[:missing_mf][:puc_id] = { mode: :direct, raise: true, keys: { farm: seq[:farm] }, msg: "PUC: #{seq[:farm]}" } if puc_id.nil?
+      rec[:missing_mf][:puc_id] = { mode: :direct, raise: true, keys: { farm: seq[:farm] }, msg: "PUC: '#{seq[:farm]}'" } if puc_id.nil?
 
       farm_id = po_repo.find_farm_id(puc_id)
       farm_or_puc_desc = if farm_id.nil?
-                           "PUC: #{seq[:farm]}"
+                           "PUC: '#{seq[:farm]}'"
                          else
-                           "farm: #{po_repo.get(:farms, :farm_code, farm_id)}"
+                           "farm: '#{po_repo.get(:farms, :farm_code, farm_id)}'"
                          end
       rec[:lookup_data][:farm_id] = farm_id
-      rec[:missing_mf][:farm_id] = { mode: :indirect, raise: true, keys: { puc_id: puc_id }, msg: "Farm for PUC: #{seq[:farm]}" } if farm_id.nil?
+      rec[:missing_mf][:farm_id] = { mode: :indirect, raise: true, keys: { puc_id: puc_id }, msg: "Farm for PUC: '#{seq[:farm]}'" } if farm_id.nil?
       orchard_id = if seq[:orchard].nil_or_empty? && AppConst::CR_EDI.create_unknown_orchard?
                      po_repo.find_unknown_orchard_id(farm_id, puc_id)
                    else
                      po_repo.find_orchard_id(farm_id, seq[:orchard])
                    end
       rec[:lookup_data][:orchard_id] = orchard_id
-      rec[:missing_mf][:orchard_id] = { mode: :direct, raise: false, keys: { farm_id: farm_id, orchard: seq[:orchard] }, msg: "Orchard: #{seq[:orchard]} for #{farm_or_puc_desc}" } if orchard_id.nil?
+      rec[:missing_mf][:orchard_id] = { mode: :direct, raise: false, keys: { farm_id: farm_id, orchard: seq[:orchard] }, msg: "Orchard: '#{seq[:orchard]}' for #{farm_or_puc_desc}" } if orchard_id.nil?
 
       marketing_variety_id = po_repo.find_marketing_variety_id(seq[:variety])
       rec[:lookup_data][:marketing_variety_id] = marketing_variety_id
-      rec[:missing_mf][:marketing_variety_id] = { mode: :direct, raise: true, keys: { variety: seq[:variety] }, msg: "Marketing variety: #{seq[:variety]}" } if marketing_variety_id.nil?
+      rec[:missing_mf][:marketing_variety_id] = { mode: :direct, raise: true, keys: { variety: seq[:variety] }, msg: "Marketing variety: '#{seq[:variety]}'" } if marketing_variety_id.nil?
       cultivar_id = po_repo.find_cultivar_id_from_mkv(marketing_variety_id)
       rec[:lookup_data][:cultivar_id] = cultivar_id
-      rec[:missing_mf][:cultivar_id] = { mode: :indirect, keys: { marketing_variety_id: marketing_variety_id }, msg: "Cultivar for variety: #{seq[:variety]}" } if cultivar_id.nil?
+      rec[:missing_mf][:cultivar_id] = { mode: :indirect, keys: { marketing_variety_id: marketing_variety_id }, msg: "Cultivar for variety: '#{seq[:variety]}'" } if cultivar_id.nil?
       cultivar_group_id = po_repo.find_cultivar_group_id(cultivar_id)
       rec[:lookup_data][:cultivar_group_id] = cultivar_group_id
-      rec[:missing_mf][:cultivar_group_id] = { mode: :indirect, keys: { cultivar_id: cultivar_id }, msg: "Cultivar Group for variety: #{seq[:variety]}" } if cultivar_group_id.nil?
+      rec[:missing_mf][:cultivar_group_id] = { mode: :indirect, keys: { cultivar_id: cultivar_id }, msg: "Cultivar Group for variety: '#{seq[:variety]}'" } if cultivar_group_id.nil?
       season_id = MasterfilesApp::CalendarRepo.new.get_season_id(cultivar_id, inspec_date || tran_date) unless cultivar_id.nil?
       season_cultivar_desc = if cultivar_id.nil?
-                               "cultivar of Marketing Variety: #{seq[:variety]}"
+                               "cultivar of Marketing Variety: '#{seq[:variety]}'"
                              else
-                               "cultivar: #{po_repo.get(:cultivars, :cultivar_code, cultivar_id)}"
+                               "cultivar: '#{po_repo.get(:cultivars, :cultivar_code, cultivar_id)}'"
                              end
       rec[:lookup_data][:season_id] = season_id
-      rec[:missing_mf][:season_id] = { mode: :direct, raise: true, keys: { date: inspec_date || tran_date, cultivar_id: cultivar_id }, msg: "Season for #{season_cultivar_desc} covering date: #{(inspec_date || tran_date).to_date}" } if season_id.nil?
+      rec[:missing_mf][:season_id] = { mode: :direct, raise: true, keys: { date: inspec_date || tran_date, cultivar_id: cultivar_id }, msg: "Season for '#{season_cultivar_desc}' covering date: '#{(inspec_date || tran_date).to_date}'" } if season_id.nil?
       marketing_org_party_role_id = MasterfilesApp::PartyRepo.new.find_party_role_from_org_code_for_role(seq[:orgzn], AppConst::ROLE_MARKETER)
       marketing_org_party_role_id = po_repo.find_variant_id(:marketing_party_roles, seq[:orgzn]) if marketing_org_party_role_id.nil?
       rec[:lookup_data][:marketing_org_party_role_id] = marketing_org_party_role_id
-      rec[:missing_mf][:marketing_org_party_role_id] = { mode: :direct, keys: { orgzn: seq[:orgzn], role: AppConst::ROLE_MARKETER }, msg: "Organization: #{seq[:orgzn]} with role: #{AppConst::ROLE_MARKETER}" } if marketing_org_party_role_id.nil?
+      rec[:missing_mf][:marketing_org_party_role_id] = { mode: :direct, keys: { orgzn: seq[:orgzn], role: AppConst::ROLE_MARKETER }, msg: "Organization: '#{seq[:orgzn]}' with role: '#{AppConst::ROLE_MARKETER}'" } if marketing_org_party_role_id.nil?
 
       # --------------------- TARGET MARKET RELATED
       targets = po_repo.find_targets(seq[:targ_mkt], seq[:target_region], seq[:target_country])
       rec[:lookup_data][:packed_tm_group_id] = targets.instance[:packed_tm_group_id]
-      rec[:missing_mf][:packed_tm_group_id] = { mode: :direct, raise: false, keys: { targ_mkt: seq[:targ_mkt] }, msg: "Target Market Group: #{seq[:targ_mkt]}" } if targets.instance[:packed_tm_group_id].nil?
+      rec[:missing_mf][:packed_tm_group_id] = { mode: :direct, raise: false, keys: { targ_mkt: seq[:targ_mkt] }, msg: "Target Market Group: '#{seq[:targ_mkt]}'" } if targets.instance[:packed_tm_group_id].nil?
       rec[:lookup_data][:target_market_id] = targets.instance[:target_market_id] unless targets.instance[:single]
 
       # The EDI targ_mkt has not been applied as a packed tm grp or target market, so we expect it to be a target customer:
@@ -284,19 +284,19 @@ module EdiApp
         target_customer_party_role_id = MasterfilesApp::PartyRepo.new.find_party_role_from_org_code_for_role(seq[:targ_mkt], AppConst::ROLE_TARGET_CUSTOMER)
         target_customer_party_role_id = po_repo.find_variant_id(:target_customer_party_roles, seq[:targ_mkt]) if target_customer_party_role_id.nil?
         rec[:lookup_data][:target_customer_party_role_id] = target_customer_party_role_id
-        rec[:missing_mf][:target_customer_party_role_id] = { mode: :direct, keys: { targ_mkt: seq[:targ_mkt], role: AppConst::ROLE_TARGET_CUSTOMER }, msg: "Organization: #{seq[:targ_mkt]} with role: #{AppConst::ROLE_TARGET_CUSTOMER}" } if target_customer_party_role_id.nil?
+        rec[:missing_mf][:target_customer_party_role_id] = { mode: :direct, keys: { targ_mkt: seq[:targ_mkt], role: AppConst::ROLE_TARGET_CUSTOMER }, msg: "Organization: '#{seq[:targ_mkt]}' with role: '#{AppConst::ROLE_TARGET_CUSTOMER}'" } if target_customer_party_role_id.nil?
       end
       # ---------------------
 
       mark_id = po_repo.find_mark_id(seq[:mark])
       rec[:lookup_data][:mark_id] = mark_id
-      rec[:missing_mf][:mark_id] = { mode: :direct, raise: false, keys: { mark: seq[:mark] }, msg: "Mark: #{seq[:mark]}" } if mark_id.nil?
+      rec[:missing_mf][:mark_id] = { mode: :direct, raise: false, keys: { mark: seq[:mark] }, msg: "Mark: '#{seq[:mark]}'" } if mark_id.nil?
       inventory_code_id = po_repo.find_inventory_code_id(seq[:inv_code] || AppConst::CR_EDI.default_edi_in_inv_code)
       rec[:lookup_data][:inventory_code_id] = inventory_code_id
-      rec[:missing_mf][:inventory_code_id] = { mode: :direct, raise: false, keys: { inv_code: seq[:inv_code] }, msg: "Inventory code: #{seq[:inv_code]}" } if inventory_code_id.nil?
+      rec[:missing_mf][:inventory_code_id] = { mode: :direct, raise: false, keys: { inv_code: seq[:inv_code] }, msg: "Inventory code: '#{seq[:inv_code]}'" } if inventory_code_id.nil?
       grade_id = po_repo.find_grade_id(seq[:grade])
       rec[:lookup_data][:grade_id] = grade_id
-      rec[:missing_mf][:grade_id] = { mode: :direct, keys: { grade: seq[:grade] }, msg: "Grade: #{seq[:grade]}" } if grade_id.nil?
+      rec[:missing_mf][:grade_id] = { mode: :direct, keys: { grade: seq[:grade] }, msg: "Grade: '#{seq[:grade]}'" } if grade_id.nil?
 
       rec[:lookup_data][:basic_pack_code_id] = parent[:lookup_data][:basic_pack_code_id]
       rec[:lookup_data][:standard_pack_code_id] = parent[:lookup_data][:standard_pack_code_id]
