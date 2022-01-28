4176d33af7341c37d42972765fa45f10774bedcf
Miracle <miraclemanzongo@gmail.com>
2022-01-28 13:59:50 +0200

    Bin Asset Report - launch bin asset report from Bin Asset Transactions Grid (#265)
    Co-authored-by: miramanz <miraclemanzongo@gmail,com>

--- routes/raw_materials/bin_assets.rb
+++ routes/raw_materials/bin_assets.rb
@@ -11,6 +11,19 @@ class Nspack < Roda
         handle_not_found(r)
       end
 
+      r.on 'preview_bin_asset_report' do
+        jasper_params = JasperParams.new('bin_asset',
+                                         current_user.login_name,
+                                         bin_asset_transaction_item_id: id)
+        res = CreateJasperReport.call(jasper_params)
+
+        if res.success
+          change_window_location_via_json(UtilityFunctions.cache_bust_url(res.instance), request.path)
+        else
+          show_error(res.message, fetch?(r))
+        end
+      end
+
       r.is do
         r.get do       # SHOW
           check_auth!('Bin Assets', 'read')
